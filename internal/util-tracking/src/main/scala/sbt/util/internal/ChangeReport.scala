dee27e95c95e6d211716e46f43975d95fc73e67f
Eugene Yokota <eed3si9n@gmail.com>
2015-09-04 12:54:09 -0400

    Move util into sbt.util.internal package

--- internal/util-tracking/src/main/scala/sbt/util/internal/ChangeReport.scala
+++ internal/util-tracking/src/main/scala/sbt/util/internal/ChangeReport.scala
@@ -1,7 +1,7 @@
 /* sbt -- Simple Build Tool
  * Copyright 2009, 2010 Mark Harrah
  */
-package sbt
+package sbt.util.internal
 
 object ChangeReport {
   def modified[T](files: Set[T]): ChangeReport[T] =
