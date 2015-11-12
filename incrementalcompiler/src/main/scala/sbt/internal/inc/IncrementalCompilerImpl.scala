c883f062fd6b1d26800b651cc6acf088e4100c02
Eugene Yokota <eed3si9n@gmail.com>
2015-11-12 00:04:23 -0500

    Add def setup

--- incrementalcompiler/src/main/scala/sbt/internal/inc/IncrementalCompilerImpl.scala
+++ incrementalcompiler/src/main/scala/sbt/internal/inc/IncrementalCompilerImpl.scala
@@ -3,9 +3,9 @@ package internal
 package inc
 
 import sbt.internal.inc.javac.{ IncrementalCompilerJavaTools, JavaTools }
-import xsbti.{ Position, Logger, Maybe }
+import xsbti.{ Position, Logger, Maybe, Reporter }
 import xsbti.compile.{ CompileOrder, GlobalsCache, IncOptions, MiniSetup, CompileAnalysis, CompileResult, CompileOptions }
-import xsbti.compile.{ PreviousResult, Setup, Inputs, IncrementalCompiler, F1 }
+import xsbti.compile.{ PreviousResult, Setup, Inputs, IncrementalCompiler, F1, DefinesClass }
 import xsbti.compile.{ Compilers => XCompilers }
 import java.io.File
 import sbt.util.Logger.m2o
@@ -38,6 +38,9 @@ class IncrementalCompilerImpl extends IncrementalCompiler {
         { f => { s => definesClass()(f)(s) } },
         reporter, order, skip, incrementalCompilerOptions)(log)
     }
+  def setup(analysisMap: F1[File, Maybe[CompileAnalysis]], definesClass: F1[File, DefinesClass], skip: Boolean, cacheFile: File, cache: GlobalsCache,
+    incrementalCompilerOptions: IncOptions, reporter: Reporter): Setup =
+    new Setup(analysisMap, definesClass, skip, cacheFile, cache, incrementalCompilerOptions, reporter)
   def inputs(options: CompileOptions, compilers: XCompilers, setup: Setup, previousResult: PreviousResult): Inputs =
     new Inputs(compilers, options, setup, previousResult)
   def inputs(classpath: Array[File], sources: Array[File], classesDirectory: File, scalacOptions: Array[String],
