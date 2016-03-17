c2a1fae4c6ef5ac7e720cf315c0c35c0a6cca489
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2016-03-17 21:26:50 +0100

    Port gkossakowski/sbt@800a01460b031790110e2c959ff474d8b9587a1b
    Not all changes are ported because scripted doesn't run inherited-macros
    test.

--- incrementalcompiler/src/sbt-test/source-dependencies/inherited_type_params/A.scala
+++
@@ -1,7 +0,0 @@
-
-trait Equal[-A] {
-  def equal(a1: A, a2: A): Boolean
-}
-object Test {
-  implicit def TraversableEqual[CC[X] <: collection.TraversableLike[X, CC[X]] with Traversable[X], A: Equal]: Equal[CC[A]] = error("")
-}
--- incrementalcompiler/src/sbt-test/source-dependencies/inherited_type_params/build.sbt
+++
@@ -1,7 +0,0 @@
-name := "test"
-
-TaskKey[Unit]("check-same") <<= compile in Configurations.Compile map { analysis =>
-	analysis.apis.internal foreach { case (_, api) =>
-		assert( xsbt.api.SameAPI(api.api, api.api) )
-	}
-}
