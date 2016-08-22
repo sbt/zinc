d4a564dea7443d13ae3c8a742b608f7858aad7aa
Krzysztof Romanowski <Krzysztof.Romanowski@morganstanley.com>
2016-08-22 16:50:02 +0200

    File base store now zips Analysis instead of writting to plain text file

--- internal/zinc-persist/src/main/scala/sbt/internal/inc/FileBasedStore.scala
+++ internal/zinc-persist/src/main/scala/sbt/internal/inc/FileBasedStore.scala
@@ -5,19 +5,49 @@ package sbt
 package internal
 package inc
 
-import java.io.File
+import java.io._
+import java.util.zip.ZipEntry
 import sbt.io.{ IO, Using }
 import xsbti.compile.{ CompileAnalysis, MiniSetup }
 
 object FileBasedStore {
   def apply(file: File): AnalysisStore = new AnalysisStore {
+
+    private val entryFileName = "cache.txt"
+
     def set(analysis: CompileAnalysis, setup: MiniSetup): Unit = {
-      Using.fileWriter(IO.utf8)(file) { writer => TextAnalysisFormat.write(writer, analysis, setup) }
+      if (!file.getParentFile.exists()) file.getParentFile.mkdirs()
+      Using.zipOutputStream(new FileOutputStream(file)) {
+        outputStream =>
+          val reader = new BufferedWriter(new OutputStreamWriter(outputStream, IO.utf8))
+          outputStream.putNextEntry(new ZipEntry(entryFileName))
+          TextAnalysisFormat.write(reader, analysis, setup)
+          outputStream.closeEntry()
+      }
     }
 
     def get(): Option[(CompileAnalysis, MiniSetup)] =
-      try { Some(getUncaught()) } catch { case _: Exception => None }
+      try {
+        Some(getUncaught())
+      } catch {
+        case _: IOException =>
+          readOldFormat()
+        case _: Exception => None
+      }
+
     def getUncaught(): (CompileAnalysis, MiniSetup) =
-      Using.fileReader(IO.utf8)(file) { reader => TextAnalysisFormat.read(reader) }
+      Using.zipInputStream(new FileInputStream(file)) {
+        inputStream =>
+          inputStream.getNextEntry()
+          val writer = new BufferedReader(new InputStreamReader(inputStream, IO.utf8))
+          TextAnalysisFormat.read(writer)
+      }
+
+    def readOldFormat(): Option[(CompileAnalysis, MiniSetup)] = try
+      Some(Using.fileReader(IO.utf8)(file) { reader => TextAnalysisFormat.read(reader) })
+    catch {
+      case _: Throwable => None
+    }
+
   }
 }
