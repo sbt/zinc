111238d6d05787499e541bb1cc04adb5898655d5
Peiyu Wang <peiyu@twitter.com>
2016-10-19 22:25:37 -0700

    Move NoopClassFileManager out of test to for other non-javac use cases

---
+++ internal/zinc-compile-core/src/main/scala/sbt/internal/inc/NoopClassFileManager.scala
@@ -0,0 +1,14 @@
+package sbt.internal.inc
+
+import java.io.File
+
+import xsbti.compile.ClassFileManager
+
+/**
+ * A noop ClassFileManager that applies to non-javac tools.
+ */
+class NoopClassFileManager extends ClassFileManager {
+  override def delete(classes: Array[File]): Unit = ()
+  override def generated(classes: Array[File]): Unit = ()
+  override def complete(success: Boolean): Unit = ()
+}
