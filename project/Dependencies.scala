dd828b95134f06a9f2cb18e424baea2d026eb164
Eugene Yokota <eed3si9n@gmail.com>
2017-02-16 15:00:32 -0500

    Bump util and lm

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,8 +11,8 @@ object Dependencies {
 
   val bootstrapSbtVersion = "0.13.8"
   private val ioVersion = "1.0.0-M9"
-  private val utilVersion = "1.0.0-M18"
-  private val lmVersion = "1.0.0-X4"
+  private val utilVersion = "1.0.0-M19"
+  private val lmVersion = "1.0.0-X5"
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
