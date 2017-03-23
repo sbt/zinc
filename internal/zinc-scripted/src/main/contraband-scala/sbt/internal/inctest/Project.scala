6bcf8e3de697ce2f0b7d7529defb80354308f022
Eugene Yokota <eed3si9n@gmail.com>
2017-03-23 17:56:38 -0400

    Add scalaVersion to zinc scripted
    Ref #276

--- internal/zinc-scripted/src/main/contraband-scala/sbt/internal/inctest/Project.scala
+++ internal/zinc-scripted/src/main/contraband-scala/sbt/internal/inctest/Project.scala
@@ -7,22 +7,23 @@ package sbt.internal.inctest
 final class Project private (
   val name: String,
   val dependsOn: Vector[String],
-  val in: Option[java.io.File]) extends Serializable {
+  val in: Option[java.io.File],
+  val scalaVersion: Option[String]) extends Serializable {
   
-  private def this(name: String) = this(name, Vector(), None)
+  private def this(name: String) = this(name, Vector(), None, None)
   
   override def equals(o: Any): Boolean = o match {
-    case x: Project => (this.name == x.name) && (this.dependsOn == x.dependsOn) && (this.in == x.in)
+    case x: Project => (this.name == x.name) && (this.dependsOn == x.dependsOn) && (this.in == x.in) && (this.scalaVersion == x.scalaVersion)
     case _ => false
   }
   override def hashCode: Int = {
-    37 * (37 * (37 * (17 + name.##) + dependsOn.##) + in.##)
+    37 * (37 * (37 * (37 * (17 + name.##) + dependsOn.##) + in.##) + scalaVersion.##)
   }
   override def toString: String = {
-    "Project(" + name + ", " + dependsOn + ", " + in + ")"
+    "Project(" + name + ", " + dependsOn + ", " + in + ", " + scalaVersion + ")"
   }
-  protected[this] def copy(name: String = name, dependsOn: Vector[String] = dependsOn, in: Option[java.io.File] = in): Project = {
-    new Project(name, dependsOn, in)
+  protected[this] def copy(name: String = name, dependsOn: Vector[String] = dependsOn, in: Option[java.io.File] = in, scalaVersion: Option[String] = scalaVersion): Project = {
+    new Project(name, dependsOn, in, scalaVersion)
   }
   def withName(name: String): Project = {
     copy(name = name)
@@ -36,10 +37,16 @@ final class Project private (
   def withIn(in: java.io.File): Project = {
     copy(in = Option(in))
   }
+  def withScalaVersion(scalaVersion: Option[String]): Project = {
+    copy(scalaVersion = scalaVersion)
+  }
+  def withScalaVersion(scalaVersion: String): Project = {
+    copy(scalaVersion = Option(scalaVersion))
+  }
 }
 object Project {
   
-  def apply(name: String): Project = new Project(name, Vector(), None)
-  def apply(name: String, dependsOn: Vector[String], in: Option[java.io.File]): Project = new Project(name, dependsOn, in)
-  def apply(name: String, dependsOn: Vector[String], in: java.io.File): Project = new Project(name, dependsOn, Option(in))
+  def apply(name: String): Project = new Project(name, Vector(), None, None)
+  def apply(name: String, dependsOn: Vector[String], in: Option[java.io.File], scalaVersion: Option[String]): Project = new Project(name, dependsOn, in, scalaVersion)
+  def apply(name: String, dependsOn: Vector[String], in: java.io.File, scalaVersion: String): Project = new Project(name, dependsOn, Option(in), Option(scalaVersion))
 }
