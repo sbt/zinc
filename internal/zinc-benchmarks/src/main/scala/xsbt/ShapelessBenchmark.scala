d061e9e99be5c8a5bd4ced89481fd0d0bec82c1a
jvican <jorgevc@fastmail.es>
2017-05-18 18:06:25 +0200

    Add headers to files that don't have them

--- internal/zinc-benchmarks/src/main/scala/xsbt/ShapelessBenchmark.scala
+++ internal/zinc-benchmarks/src/main/scala/xsbt/ShapelessBenchmark.scala
@@ -1,3 +1,9 @@
+/*
+ * Zinc - The incremental compiler for Scala.
+ * Copyright 2011 - 2017, Lightbend, Inc.
+ * Copyright 2008 - 2010, Mark Harrah
+ * This software is released under the terms written in LICENSE.
+ */
 /*package xsbt
 
 import java.util.concurrent.TimeUnit
