86de228744abe9a34d054de8a3560fa2c5200edb
jvican <jorgevc@fastmail.es>
2017-06-29 23:43:16 +0200

    Merge `CommonData` & `Feedback` into `Defaults`
    No need to have those extra objects lying around. New design is more
    consistent.

--- internal/zinc-persist/src/main/scala/sbt/internal/inc/converters/ProtobufDefaults.scala
+++ internal/zinc-persist/src/main/scala/sbt/internal/inc/converters/ProtobufDefaults.scala
@@ -3,7 +3,28 @@ package sbt.internal.inc.converters
 import sbt.internal.inc.schema
 
 object ProtobufDefaults {
+  final val MissingInt: Int = -1
+  final val MissingString: String = ""
+
   final val ThisQualifier = schema.ThisQualifier.defaultInstance
   final val Unqualified = schema.Unqualified.defaultInstance
   final val PublicAccess = schema.Public.defaultInstance
+
+  object Feedback {
+    object Writers {
+      final val ExpectedNonEmptyOutput =
+        "Expected `Output` to be either `SingleOutput` or `MultipleOutput`."
+    }
+
+    object Readers {
+      final def unrecognizedSeverity(id: Int) =
+        s"Unrecognized `Severity` level when reading data (id = $id)."
+      final def unrecognizedOrder(id: Int) =
+        s"Unrecognized `CompileOrder` when reading data (id = $id)."
+      final val MissingMiniOptions = "`MiniOptions` are missing in `MiniSetup` when reading data."
+      final val ExpectedNonEmptyOutput = "Expected non-empty `Output` when reading data."
+      final val ExpectedPositionInProblem: String =
+        "Expected non-empty `Position` in `Problem` when reading data."
+    }
+  }
 }
