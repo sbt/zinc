18904b7897b2255c87dd0262facdc01f8a44d096
jvican <jorgevc@fastmail.es>
2017-07-04 13:44:24 +0200

    Add writer for Analysis and support the missing API
    This commit adds support to write the analysis instance with Protobuf.
    
    The API was not properly covered before, missing some of the most
    important protos: `Relations` et al. The current implementation of the
    `Relations` writer does not sort the results as the previous text
    analysis format did because order is not required for efficient
    serialization/deserialization.

--- internal/zinc-persist/src/main/scala/sbt/internal/inc/converters/ProtobufWriters.scala
+++ internal/zinc-persist/src/main/scala/sbt/internal/inc/converters/ProtobufWriters.scala
@@ -2,8 +2,8 @@ package sbt.internal.inc.converters
 
 import java.io.File
 
-import sbt.internal.inc.{ Compilation, Compilations, Hash, LastModified, Stamps, schema }
-import xsbti.{ Position, Problem, Severity, T2 }
+import sbt.internal.inc._
+import xsbti.{ Position, Problem, Severity, T2, UseScope }
 import xsbti.compile.analysis.{ SourceInfo, Stamp }
 import sbt.internal.inc.converters.ProtobufDefaults.Feedback.{ Writers => WritersFeedback }
 import sbt.internal.inc.converters.ProtobufDefaults.WritersConstants
@@ -121,6 +121,11 @@ object ProtobufWriters {
     )
   }
 
+  def toSourceInfos(sourceInfos0: SourceInfos): schema.SourceInfos = {
+    val sourceInfos = sourceInfos0.allInfos.map(kv => kv._1.getAbsolutePath -> toSourceInfo(kv._2))
+    schema.SourceInfos(sourceInfos = sourceInfos)
+  }
+
   def toFileHash(fileHash: FileHash): schema.FileHash = {
     schema.FileHash(
       path = fileHash.file.getAbsolutePath,
@@ -215,6 +220,17 @@ object ProtobufWriters {
     schema.Annotation(base = base, arguments = arguments)
   }
 
+  def toStructure(tpe: Structure): schema.Type.Structure = {
+    val declared = tpe.declared().toSchemaList(toClassDefinition)
+    val inherited = tpe.inherited().toSchemaList(toClassDefinition)
+    val parents = tpe.parents().toSchemaList(toType)
+    schema.Type.Structure(
+      declared = declared,
+      inherited = inherited,
+      parents = parents
+    )
+  }
+
   def toType(`type`: Type): schema.Type = {
     def toExistential(tpe: Existential): schema.Type.Existential = {
       val baseType = Some(toType(tpe.baseType()))
@@ -261,17 +277,6 @@ object ProtobufWriters {
       schema.Type.Annotated(baseType = baseType, annotations = annotations)
     }
 
-    def toStructure(tpe: Structure): schema.Type.Structure = {
-      val declared = tpe.declared().toSchemaList(toClassDefinition)
-      val inherited = tpe.inherited().toSchemaList(toClassDefinition)
-      val parents = tpe.parents().toSchemaList(toType)
-      schema.Type.Structure(
-        declared = declared,
-        inherited = inherited,
-        parents = parents
-      )
-    }
-
     val schemaType = `type` match {
       case tpe: ParameterRef  => schema.Type.Value.ParameterRef(value = toParameterRef(tpe))
       case tpe: Parameterized => schema.Type.Value.Parameterized(value = toParameterized(tpe))
@@ -288,34 +293,45 @@ object ProtobufWriters {
     schema.Type(value = schemaType)
   }
 
-  def toClassDefinition(classDefinition: ClassDefinition): schema.ClassDefinition = {
-    def toAccess(access: Access): schema.Access = {
-      def toQualifier(qualifier: Qualifier): schema.Qualifier = {
-        import WritersConstants.{ ThisQualifier, Unqualified }
-        import schema.Qualifier.{ Type => QualifierType }
-        val qualifierType = qualifier match {
-          case q: IdQualifier   => QualifierType.IdQualifier(value = schema.IdQualifier(q.value()))
-          case _: ThisQualifier => QualifierType.ThisQualifier(value = ThisQualifier)
-          case _: Unqualified   => QualifierType.Unqualified(value = Unqualified)
-        }
-        schema.Qualifier(`type` = qualifierType)
-      }
-      import WritersConstants.PublicAccess
-      import schema.Access.{ Type => AccessType }
-      val accessType = access match {
-        case _: Public => AccessType.Public(value = PublicAccess)
-        case qualified: Qualified =>
-          val qualifier = Some(toQualifier(qualified.qualifier()))
-          qualified match {
-            case _: Private   => AccessType.Private(schema.Private(qualifier = qualifier))
-            case _: Protected => AccessType.Protected(schema.Protected(qualifier = qualifier))
-          }
-      }
-      schema.Access(`type` = accessType)
+  def toDefinitionType(definitionType: DefinitionType): schema.DefinitionType = {
+    definitionType match {
+      case DefinitionType.ClassDef      => schema.DefinitionType.CLASSDEF
+      case DefinitionType.Module        => schema.DefinitionType.MODULE
+      case DefinitionType.Trait         => schema.DefinitionType.TRAIT
+      case DefinitionType.PackageModule => schema.DefinitionType.PACKAGEMODULE
     }
+  }
 
-    def toModifiers(modifiers: Modifiers): schema.Modifiers =
-      schema.Modifiers(flags = modifiers.raw().toInt)
+  def toAccess(access: Access): schema.Access = {
+    def toQualifier(qualifier: Qualifier): schema.Qualifier = {
+      import WritersConstants.{ ThisQualifier, Unqualified }
+      import schema.Qualifier.{ Type => QualifierType }
+      val qualifierType = qualifier match {
+        case q: IdQualifier   => QualifierType.IdQualifier(value = schema.IdQualifier(q.value()))
+        case _: ThisQualifier => QualifierType.ThisQualifier(value = ThisQualifier)
+        case _: Unqualified   => QualifierType.Unqualified(value = Unqualified)
+      }
+      schema.Qualifier(`type` = qualifierType)
+    }
+    import WritersConstants.PublicAccess
+    import schema.Access.{ Type => AccessType }
+    val accessType = access match {
+      case _: Public => AccessType.Public(value = PublicAccess)
+      case qualified: Qualified =>
+        val qualifier = Some(toQualifier(qualified.qualifier()))
+        qualified match {
+          case _: Private   => AccessType.Private(schema.Private(qualifier = qualifier))
+          case _: Protected => AccessType.Protected(schema.Protected(qualifier = qualifier))
+        }
+    }
+    schema.Access(`type` = accessType)
+  }
+
+  def toModifiers(modifiers: Modifiers): schema.Modifiers = {
+    schema.Modifiers(flags = modifiers.raw().toInt)
+  }
+
+  def toClassDefinition(classDefinition: ClassDefinition): schema.ClassDefinition = {
 
     def toParameterList(parameterList: ParameterList): schema.ParameterList = {
       def toMethodParameter(methodParameter: MethodParameter): schema.MethodParameter = {
@@ -345,14 +361,6 @@ object ProtobufWriters {
 
     import schema.ClassDefinition.{ Extra => DefType }
     def toClassLikeDef(classLikeDef: ClassLikeDef): DefType.ClassLikeDef = {
-      def toDefinitionType(definitionType: DefinitionType): schema.DefinitionType = {
-        definitionType match {
-          case DefinitionType.ClassDef      => schema.DefinitionType.CLASSDEF
-          case DefinitionType.Module        => schema.DefinitionType.MODULE
-          case DefinitionType.Trait         => schema.DefinitionType.TRAIT
-          case DefinitionType.PackageModule => schema.DefinitionType.PACKAGEMODULE
-        }
-      }
       val definitionType = toDefinitionType(classLikeDef.definitionType())
       val typeParameters = classLikeDef.typeParameters().toSchemaList(toTypeParameter)
       DefType.ClassLikeDef(
@@ -454,6 +462,164 @@ object ProtobufWriters {
       variance = variance,
       lowerBound = lowerBound,
       upperBound = upperBound
+    )
+  }
+
+  def toClassLike(classLike: ClassLike): schema.ClassLike = {
+    val name = classLike.name()
+    val access = Some(toAccess(classLike.access()))
+    val modifiers = Some(toModifiers(classLike.modifiers()))
+    val annotations = classLike.annotations().toSchemaList(toAnnotation)
+
+    val definitionType = toDefinitionType(classLike.definitionType())
+    val selfType = Some(toType(classLike.selfType()))
+    val structure = Some(toStructure(classLike.structure()))
+    val savedAnnotations = classLike.savedAnnotations()
+    val childrenOfSealedClass = classLike.childrenOfSealedClass().toSchemaList(toType)
+    val topLevel = classLike.topLevel()
+    val typeParameters = classLike.typeParameters().toSchemaList(toTypeParameter)
+    schema.ClassLike(
+      name = name,
+      access = access,
+      modifiers = modifiers,
+      annotations = annotations,
+      definitionType = definitionType,
+      selfType = selfType,
+      structure = structure,
+      savedAnnotations = savedAnnotations,
+      childrenOfSealedClass = childrenOfSealedClass,
+      topLevel = topLevel,
+      typeParameters = typeParameters
+    )
+  }
+
+  def toUseScope(useScope: UseScope): schema.UseScope = {
+    useScope match {
+      case UseScope.Default      => schema.UseScope.DEFAULT
+      case UseScope.Implicit     => schema.UseScope.IMPLICIT
+      case UseScope.PatMatTarget => schema.UseScope.PATMAT
+    }
+  }
+
+  def toAnalyzedClass(analyzedClass: AnalyzedClass): schema.AnalyzedClass = {
+    def toCompanions(companions: Companions): schema.Companions = {
+      val classApi = Some(toClassLike(companions.classApi()))
+      val objectApi = Some(toClassLike(companions.objectApi()))
+      schema.Companions(classApi = classApi, objectApi = objectApi)
+    }
+
+    def toNameHash(nameHash: NameHash): schema.NameHash = {
+      val name = nameHash.name()
+      val useScope = toUseScope(nameHash.scope())
+      schema.NameHash(name = name, scope = useScope)
+    }
+
+    val companions = Some(toCompanions(analyzedClass.api()))
+    val apiHash = analyzedClass.apiHash()
+    val compilationTimestamp = analyzedClass.compilationTimestamp()
+    val hasMacro = analyzedClass.hasMacro
+    val name = analyzedClass.name()
+    val nameHashes = analyzedClass.nameHashes().toSchemaList(toNameHash)
+    schema.AnalyzedClass(
+      compilationTimestamp = compilationTimestamp,
+      name = name,
+      api = companions,
+      apiHash = apiHash,
+      nameHashes = nameHashes,
+      hasMacro = hasMacro
+    )
+  }
+
+  private final val fileToString = (f: File) => f.getAbsolutePath
+  private final val stringId = identity[String] _
+  def toRelations(relations: Relations): schema.Relations = {
+    import sbt.internal.util.Relation
+
+    def toUsedName(usedName: UsedName): schema.UsedName = {
+      import scala.collection.JavaConverters._
+      val name = usedName.name
+      val javaIterator = usedName.scopes.iterator()
+      val scopes = javaIterator.asScala.map(toUseScope).toList
+      schema.UsedName(name = name, scopes = scopes)
+    }
+
+    def toUsedNamesMap(relation: Relation[String, UsedName]): Map[String, schema.UsedNames] = {
+      relation.forwardMap.mapValues { names =>
+        val usedNames = names.iterator.map(toUsedName).toList
+        schema.UsedNames(usedNames = usedNames)
+      }
+    }
+
+    def toMap[K, V](
+        relation: Relation[K, V],
+        fk: K => String,
+        fv: V => String
+    ): Map[String, schema.Values] = {
+      relation.forwardMap.map {
+        case (k, vs) =>
+          val fileValues = schema.Values(values = vs.iterator.map(fv).toList)
+          fk(k) -> fileValues
+      }
+    }
+
+    val srcProd = toMap(relations.srcProd, fileToString, fileToString)
+    val libraryDep = toMap(relations.libraryDep, fileToString, fileToString)
+    val libraryClassName = toMap(relations.libraryClassName, fileToString, stringId)
+    val memberRefInternal = toMap(relations.memberRef.internal, stringId, stringId)
+    val memberRefExternal = toMap(relations.memberRef.external, stringId, stringId)
+    val inheritanceInternal = toMap(relations.inheritance.internal, stringId, stringId)
+    val inheritanceExternal = toMap(relations.inheritance.external, stringId, stringId)
+    val localInheritanceInternal = toMap(relations.localInheritance.internal, stringId, stringId)
+    val localInheritanceExternal = toMap(relations.localInheritance.external, stringId, stringId)
+    val classes = toMap(relations.classes, fileToString, stringId)
+    val productClassName = toMap(relations.productClassName, stringId, stringId)
+    val names = toUsedNamesMap(relations.names)
+    val memberRef = Some(
+      schema.ClassDependencies(
+        internal = memberRefInternal,
+        external = memberRefExternal
+      ))
+    val inheritance = Some(
+      schema.ClassDependencies(
+        internal = inheritanceInternal,
+        external = inheritanceExternal
+      ))
+    val localInheritance = Some(
+      schema.ClassDependencies(
+        internal = localInheritanceInternal,
+        external = localInheritanceExternal
+      ))
+    schema.Relations(
+      srcProd = srcProd,
+      libraryDep = libraryDep,
+      libraryClassName = libraryClassName,
+      classes = classes,
+      productClassName = productClassName,
+      names = names,
+      memberRef = memberRef,
+      inheritance = inheritance,
+      localInheritance = localInheritance
+    )
+  }
+
+  def toAnalysis(analysis: Analysis): schema.Analysis = {
+    def toApis(apis: APIs): schema.APIs = {
+      val internal = apis.internal.mapValues(toAnalyzedClass)
+      val external = apis.external.mapValues(toAnalyzedClass)
+      schema.APIs(internal = internal, external = external)
+    }
+
+    val stamps = Some(toStamps(analysis.stamps))
+    val apis = Some(toApis(analysis.apis))
+    val relations = Some(toRelations(analysis.relations))
+    val sourceInfos = Some(toSourceInfos(analysis.infos))
+    val compilations = Some(toCompilations(analysis.compilations))
+    schema.Analysis(
+      stamps = stamps,
+      apis = apis,
+      relations = relations,
+      sourceInfos = sourceInfos,
+      compilations = compilations
     )
   }
 }
