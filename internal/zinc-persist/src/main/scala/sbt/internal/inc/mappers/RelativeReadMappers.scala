690b7a4336756812e8ed53470ee660c64f4f7880
jvican <jorgevc@fastmail.es>
2017-07-13 15:50:49 +0200

    Add generic read and write mappers
    This class has been added as per Stu Hood's request. It's a prototype
    that encapsulates a fine-grained abstraction over the different kind of
    root paths that Zinc handles. Based on these roots, it allows users to
    provide concrete implementations of them and will be used by Zinc in the
    appropriate places.
    
    See https://github.com/sbt/zinc/issues/320.

---
+++ internal/zinc-persist/src/main/scala/sbt/internal/inc/mappers/RelativeReadMappers.scala
@@ -0,0 +1,74 @@
+/*
+ * Zinc - The incremental compiler for Scala.
+ * Copyright 2011 - 2017, Lightbend, Inc.
+ * Copyright 2008 - 2010, Mark Harrah
+ * This software is released under the terms written in LICENSE.
+ */
+
+package sbt.internal.inc.mappers
+
+import java.io.File
+import java.nio.file.Path
+
+import xsbti.compile.MiniSetup
+import xsbti.compile.analysis.{ ReadMapper, RootPaths, Stamp }
+
+// Please see docs about the implementation in the WriteMapper interface
+/** @inheritdoc */
+final class NaiveRelativeReadMapper(rootProjectPath: Path) extends ReadMapper {
+  private def reconstructRelative(file: File): File =
+    MapperUtils.reconstructRelative(file, rootProjectPath)
+
+  override def mapSourceFile(sourceFile: File): File = reconstructRelative(sourceFile)
+  override def mapBinaryFile(binaryFile: File): File = reconstructRelative(binaryFile)
+  override def mapProductFile(productFile: File): File = reconstructRelative(productFile)
+
+  override def mapClasspathEntry(classpathEntry: File): File = reconstructRelative(classpathEntry)
+  override def mapJavacOption(javacOption: String): String = javacOption
+  override def mapScalacOption(scalacOption: String): String = scalacOption
+
+  override def mapOutputDir(outputDir: File): File = reconstructRelative(outputDir)
+  override def mapSourceDir(sourceDir: File): File = reconstructRelative(sourceDir)
+
+  override def mapProductStamp(file: File, productStamp: Stamp): Stamp = productStamp
+  override def mapSourceStamp(file: File, sourceStamp: Stamp): Stamp = sourceStamp
+  override def mapBinaryStamp(file: File, binaryStamp: Stamp): Stamp = binaryStamp
+
+  override def mapMiniSetup(miniSetup: MiniSetup): MiniSetup = miniSetup
+}
+
+final class RelativeReadMapper(rootPaths: RootPaths) extends ReadMapper {
+  import MapperUtils.reconstructRelative
+  private final val sourcesRoot = rootPaths.getSourcesRootPath.toPath
+  private final val librariesRoot = rootPaths.getLibrariesRootPath.toPath
+  private final val productsRoot = rootPaths.getProductsRootPath.toPath
+
+  override def mapSourceFile(sourceFile: File): File = reconstructRelative(sourceFile, sourcesRoot)
+  override def mapBinaryFile(binaryFile: File): File =
+    reconstructRelative(binaryFile, librariesRoot)
+  override def mapProductFile(productFile: File): File =
+    reconstructRelative(productFile, productsRoot)
+
+  /**
+   *
+   * This function reconstructs a relative path to use the libraries root, assuming
+   * that all the classpath entries are absolute (even compiler plugins) and they
+   * come from a unique place.
+   *
+   * @param classpathEntry The classpath entry to be passed to the compiler.
+   */
+  override def mapClasspathEntry(classpathEntry: File): File =
+    reconstructRelative(classpathEntry, librariesRoot)
+
+  override def mapJavacOption(javacOption: String): String = javacOption
+  override def mapScalacOption(scalacOption: String): String = scalacOption
+
+  override def mapOutputDir(outputDir: File): File = reconstructRelative(outputDir, productsRoot)
+  override def mapSourceDir(sourceDir: File): File = reconstructRelative(sourceDir, sourcesRoot)
+
+  override def mapProductStamp(file: File, productStamp: Stamp): Stamp = productStamp
+  override def mapSourceStamp(file: File, sourceStamp: Stamp): Stamp = sourceStamp
+  override def mapBinaryStamp(file: File, binaryStamp: Stamp): Stamp = binaryStamp
+
+  override def mapMiniSetup(miniSetup: MiniSetup): MiniSetup = miniSetup
+}
---
+++ internal/zinc-persist/src/main/scala/sbt/internal/inc/mappers/RelativeWriteMappers.scala
@@ -0,0 +1,70 @@
+/*
+ * Zinc - The incremental compiler for Scala.
+ * Copyright 2011 - 2017, Lightbend, Inc.
+ * Copyright 2008 - 2010, Mark Harrah
+ * This software is released under the terms written in LICENSE.
+ */
+
+package sbt.internal.inc.mappers
+
+import java.io.File
+import java.nio.file.Path
+
+import xsbti.compile.MiniSetup
+import xsbti.compile.analysis.{ RootPaths, Stamp, WriteMapper }
+
+// Please see docs about the implementation in the WriteMapper interface
+/** @inheritdoc */
+final class NaiveRelativeWriteMapper(rootProjectPath: Path) extends WriteMapper {
+  private def makeRelative(file: File): File = MapperUtils.makeRelative(file, rootProjectPath)
+
+  override def mapSourceFile(sourceFile: File): File = makeRelative(sourceFile)
+  override def mapBinaryFile(binaryFile: File): File = makeRelative(binaryFile)
+  override def mapProductFile(productFile: File): File = makeRelative(productFile)
+
+  override def mapClasspathEntry(classpathEntry: File): File = makeRelative(classpathEntry)
+  override def mapJavacOption(javacOption: String): String = javacOption
+  override def mapScalacOption(scalacOption: String): String = scalacOption
+
+  override def mapOutputDir(outputDir: File): File = makeRelative(outputDir)
+  override def mapSourceDir(sourceDir: File): File = makeRelative(sourceDir)
+
+  override def mapProductStamp(file: File, productStamp: Stamp): Stamp = productStamp
+  override def mapSourceStamp(file: File, sourceStamp: Stamp): Stamp = sourceStamp
+  override def mapBinaryStamp(file: File, binaryStamp: Stamp): Stamp = binaryStamp
+
+  override def mapMiniSetup(miniSetup: MiniSetup): MiniSetup = miniSetup
+}
+
+final class RelativeWriteMapper(rootPaths: RootPaths) extends WriteMapper {
+  import MapperUtils.makeRelative
+  private final val sourcesRoot = rootPaths.getSourcesRootPath.toPath
+  private final val librariesRoot = rootPaths.getLibrariesRootPath.toPath
+  private final val productsRoot = rootPaths.getProductsRootPath.toPath
+
+  override def mapSourceFile(sourceFile: File): File = makeRelative(sourceFile, sourcesRoot)
+  override def mapBinaryFile(binaryFile: File): File = makeRelative(binaryFile, librariesRoot)
+  override def mapProductFile(productFile: File): File = makeRelative(productFile, productsRoot)
+
+  /**
+   * This function makes a path relative to the libraries root, assuming that all
+   * the classpath entries are absolute (even compiler plugins) and they come from
+   * a unique place.
+   *
+   * @param classpathEntry The classpath entry to be passed to the compiler.
+   */
+  override def mapClasspathEntry(classpathEntry: File): File =
+    makeRelative(classpathEntry, librariesRoot)
+
+  override def mapJavacOption(javacOption: String): String = javacOption
+  override def mapScalacOption(scalacOption: String): String = scalacOption
+
+  override def mapOutputDir(outputDir: File): File = makeRelative(outputDir, productsRoot)
+  override def mapSourceDir(sourceDir: File): File = makeRelative(sourceDir, sourcesRoot)
+
+  override def mapProductStamp(file: File, productStamp: Stamp): Stamp = productStamp
+  override def mapSourceStamp(file: File, sourceStamp: Stamp): Stamp = sourceStamp
+  override def mapBinaryStamp(file: File, binaryStamp: Stamp): Stamp = binaryStamp
+
+  override def mapMiniSetup(miniSetup: MiniSetup): MiniSetup = miniSetup
+}
