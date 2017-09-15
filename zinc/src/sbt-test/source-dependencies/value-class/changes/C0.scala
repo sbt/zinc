0a6d043cca5707cae1469d1d6a108b77cc1fe673
MuxaHuk <muxanick@gmail.com>
2017-09-15 13:40:34 +0100

    Revert "Added colors and OK/FAIL text to the value-class test"
    This reverts commit 858d56370c73804d40dffcf83dfc2fd182cf1ce3.

--- zinc/src/sbt-test/source-dependencies/value-class/changes/C0.scala
+++ zinc/src/sbt-test/source-dependencies/value-class/changes/C0.scala
@@ -1,7 +1,3 @@
 object C extends App {
-  val res = new B().foo(null)
-  res match {
-    case 1 => println(Console.GREEN + "OK: " + res.toString + Console.RESET)
-    case _ => println(Console.RED + "FAIL: " + res.toString + Console.RESET)
-  }
+  println(new B().foo(null))
 }
--- zinc/src/sbt-test/source-dependencies/value-class/changes/C1.scala
+++ zinc/src/sbt-test/source-dependencies/value-class/changes/C1.scala
@@ -1,7 +1,3 @@
 object C extends App {
-  val res = new B().bar.x
-  res match {
-    case 0 => println(Console.GREEN + "OK: " + res.toString + Console.RESET)
-    case _ => println(Console.RED + "FAIL: " + res.toString + Console.RESET)
-  }
+  println(new B().bar.x)
 }
