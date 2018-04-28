93f8f1e0daeb4114ac272573e78e1521864bfe32
Eugene Yokota <eed3si9n@gmail.com>
2018-04-28 04:24:04 -0400

    Scala 2.12.6 and other dependencies
    sbt 1.1.4
    Scala 2.12.6
    ScalaCheck 1.14.0
    ScalaTest 3.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.contraband.ContrabandPlugin.autoImport._
 object Dependencies {
   val scala210 = "2.10.7"
   val scala211 = "2.11.12"
-  val scala212 = "2.12.4"
+  val scala212 = "2.12.6"
   val scala213 = "2.13.0-M2"
 
   private val ioVersion = "1.1.4"
@@ -77,8 +77,8 @@ object Dependencies {
 
   val parserCombinator = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.5"
   val sbinary = "org.scala-sbt" %% "sbinary" % "0.4.4"
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.4"
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.1"
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.0.5"
   val junit = "junit" % "junit" % "4.11"
   val sjsonnew = Def.setting {
     "com.eed3si9n" %% "sjson-new-core" % contrabandSjsonNewVersion.value
