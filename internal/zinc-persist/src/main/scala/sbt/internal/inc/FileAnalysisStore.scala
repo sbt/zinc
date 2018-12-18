e14ecce62d4f6a08eb6c0efa86d67896083560ae
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2018-12-18 12:50:30 +0100

    Allow disabling compression when saving analysis

--- internal/zinc-persist/src/main/scala/sbt/internal/inc/FileAnalysisStore.scala
+++ internal/zinc-persist/src/main/scala/sbt/internal/inc/FileAnalysisStore.scala
@@ -11,7 +11,7 @@ package inc
 
 import java.io._
 import java.util.Optional
-import java.util.zip.{ ZipEntry, ZipInputStream }
+import java.util.zip.{ ZipOutputStream, ZipEntry, Deflater, ZipInputStream }
 
 import com.google.protobuf.{ CodedInputStream, CodedOutputStream }
 import sbt.internal.inc.binary.BinaryAnalysisFormat
@@ -45,6 +45,7 @@ object FileAnalysisStore {
 
     private final val format = new BinaryAnalysisFormat(readWriteMappers)
     private final val TmpEnding = ".tmp"
+    private final val useCompression = sys.props.get("zinc.binary-file-store.use-compression").fold(true)(_ == "true")
 
     /**
      * Get `CompileAnalysis` and `MiniSetup` instances for current `Analysis`.
@@ -82,6 +83,10 @@ object FileAnalysisStore {
 
       val outputStream = new FileOutputStream(tmpAnalysisFile)
       Using.zipOutputStream(outputStream) { outputStream =>
+        if (!useCompression) {
+          outputStream.setMethod(ZipOutputStream.DEFLATED)
+          outputStream.setLevel(Deflater.NO_COMPRESSION)
+        }
         val protobufWriter = CodedOutputStream.newInstance(outputStream)
         outputStream.putNextEntry(new ZipEntry(analysisFileName))
         format.write(protobufWriter, analysis, setup)
