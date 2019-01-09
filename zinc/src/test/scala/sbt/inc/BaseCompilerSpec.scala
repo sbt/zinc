7af51ca4bf8a5fa0d03037c660b3a9f9c3f9ed3e
Stu Hood <stuhood@twitter.com>
2019-01-09 15:27:45 -0800

    Preserve lastModified in BaseCompilerSpec.

--- zinc/src/test/scala/sbt/inc/BaseCompilerSpec.scala
+++ zinc/src/test/scala/sbt/inc/BaseCompilerSpec.scala
@@ -18,7 +18,7 @@ import java.util.Optional
 
 import sbt.internal.inc._
 import sbt.internal.inc.classpath.ClassLoaderCache
-import sbt.io.IO
+import sbt.io.{ IO, CopyOptions }
 import sbt.io.syntax._
 import sbt.util.{ InterfaceUtil, Logger }
 import xsbti.compile.{ ScalaInstance => _, _ }
@@ -28,6 +28,9 @@ class BaseCompilerSpec extends BridgeProviderSpecification {
 
   val scalaVersion = scala.util.Properties.versionNumberString
   val maxErrors = 100
+
+  private[this] val copyOptions =
+    CopyOptions().withPreserveLastModified(true).withPreserveExecutable(true)
 
   case class MockedLookup(am: File => Optional[CompileAnalysis]) extends PerClasspathEntryLookup {
     override def analysis(classpathEntry: File): Optional[CompileAnalysis] =
@@ -59,7 +62,7 @@ class BaseCompilerSpec extends BridgeProviderSpecification {
       sourceFile <- sourceFiles
     } yield {
       val targetFile = sourceRoot.resolve(sourceFile).toFile
-      IO.copyFile(fromResource(sourcesPrefix)(sourceFile), targetFile)
+      IO.copyFile(fromResource(sourcesPrefix)(sourceFile), targetFile, copyOptions)
       targetFile
     }
     val classpathBase = baseLocation.resolve("bin")
@@ -73,7 +76,7 @@ class BaseCompilerSpec extends BridgeProviderSpecification {
         existingFile.toFile
       case jarPath =>
         val newJar = classpathBase.resolve(jarPath).toFile
-        IO.copyFile(fromResource(binPrefix)(jarPath), newJar)
+        IO.copyFile(fromResource(binPrefix)(jarPath), newJar, copyOptions)
         newJar
     }
 
