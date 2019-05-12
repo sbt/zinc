1b84b32bcc373f7677673d3f425ee2c585be6a9f
Eugene Yokota <eed3si9n@gmail.com>
2019-05-12 18:48:29 -0400

    use Travis CI

--- bin/run-ci.sh
+++ bin/run-ci.sh
@@ -5,7 +5,7 @@ set -o nounset
 PROJECT_ROOT="zincRoot"
 sbt -Dfile.encoding=UTF-8 \
   -J-XX:ReservedCodeCacheSize=512M \
-  -J-Xms1024M -J-Xmx4096M -J-server \
+  -J-Xms1024M -J-Xmx2048M -J-server \
   "$PROJECT_ROOT/mimaReportBinaryIssues" \
   scalafmtCheckAll \
   scalafmtSbtCheck \
