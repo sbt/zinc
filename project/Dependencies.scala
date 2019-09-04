39b27d68b830cdf0c030571b96068a80135adb66
Eugene Yokota <eed3si9n@gmail.com>
2019-09-03 22:30:15 -0400

    io 1.3.0, util 1.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,8 +9,8 @@ object Dependencies {
   val scala212 = "2.12.8"
   val scala213 = "2.13.0"
 
-  private val ioVersion = nightlyVersion.getOrElse("1.3.0-M12")
-  private val utilVersion = nightlyVersion.getOrElse("1.3.0-M8")
+  private val ioVersion = nightlyVersion.getOrElse("1.3.0")
+  private val utilVersion = nightlyVersion.getOrElse("1.3.0")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
