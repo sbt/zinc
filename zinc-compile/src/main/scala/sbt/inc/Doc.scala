ece7084aeeaa83fbf3a961d222a834e82d36820d
Dale Wijnand <dale.wijnand@gmail.com>
2020-05-06 15:31:27 +0100

    Cleanup Doc & DocSpec
    Also delete an extra, unused, UnitSpec.

--- zinc-compile/src/main/scala/sbt/inc/Doc.scala
+++ zinc-compile/src/main/scala/sbt/inc/Doc.scala
@@ -23,128 +23,66 @@ import sbt.internal.inc.javac.JavaCompilerArguments
 import sbt.util.Tracked.inputChanged
 import sbt.util.{ CacheStoreFactory, FileInfo, FilesInfo, ModifiedFileInfo, PlainFileInfo }
 import sbt.util.CacheImplicits._
-import sbt.util.FileInfo.{ exists, lastModified }
-import exists._
+import sbt.io.IO
 import sjsonnew._
 import xsbti.Reporter
 
 object Doc {
   private[this] implicit val IsoInputs = LList.iso(
-    { in: Inputs =>
-      ("getOutputDirectory", in.outputDirectory.toFile) :*: LNil
-    }, { in: File :*: LNil =>
-      Inputs(Nil, Nil, Nil, in.head.toPath, Nil)
-    }
+    (in: Inputs) => ("getOutputDirectory", in.outputDirectory.toFile) :*: LNil,
+    (in: File :*: LNil) => Inputs(Nil, Nil, Nil, in.head.toPath, Nil)
   )
 
-  // def scaladoc(label: String, cache: File, compiler: AnalyzingCompiler): RawCompileLike.Gen =
-  //   scaladoc(label, cache, compiler, Seq())
-  // def scaladoc(label: String, cache: File, compiler: AnalyzingCompiler, fileInputOptions: Seq[String]): RawCompileLike.Gen =
-  //   RawCompileLike.cached(cache, fileInputOptions, RawCompileLike.prepare(label + " Scala API documentation", compiler.doc))
   def cachedJavadoc(label: String, storeFactory: CacheStoreFactory, doc: JavaTools): JavaDoc =
     cached(
       storeFactory,
       prepare(
-        label + " Java API documentation",
-        new JavaDoc {
-          def run(
-              sources: List[VirtualFile],
-              classpath: List[VirtualFile],
-              converter: FileConverter,
-              outputDirectory: Path,
-              options: List[String],
-              incToolOptions: IncToolOptions,
-              log: Logger,
-              reporter: Reporter
-          ): Unit = {
-            val success = doc.javadoc.run(
-              (sources filter javaSourcesOnly).toArray,
-              JavaCompilerArguments(Nil, classpath, options).toArray,
-              CompileOutput(outputDirectory),
-              incToolOptions,
-              reporter,
-              log
-            )
-            if (success) ()
-            else throw new JavadocGenerationFailed()
-          }
+        s"$label Java API documentation",
+        (sources, classpath, _, outputDirectory, options, incToolOptions, log, reporter) => {
+          val success = doc.javadoc.run(
+            sources.filter(javaSourcesOnly).toArray,
+            JavaCompilerArguments(Nil, classpath, options).toArray,
+            CompileOutput(outputDirectory),
+            incToolOptions,
+            reporter,
+            log
+          )
+          if (!success)
+            throw new JavadocGenerationFailed()
         }
       )
     )
+
   private[sbt] def prepare(description: String, doDoc: JavaDoc): JavaDoc =
-    new JavaDoc {
-      def run(
-          sources: List[VirtualFile],
-          classpath: List[VirtualFile],
-          converter: FileConverter,
-          outputDirectory: Path,
-          options: List[String],
-          incToolOptions: IncToolOptions,
-          log: Logger,
-          reporter: Reporter
-      ): Unit =
-        if (sources.isEmpty) log.info("No sources available, skipping " + description + "...")
-        else {
-          log.info(
-            description.capitalize + " to " + outputDirectory.toAbsolutePath.toString + "..."
-          )
-          doDoc.run(
-            sources,
-            classpath,
-            converter,
-            outputDirectory,
-            options,
-            incToolOptions,
-            log,
-            reporter
-          )
-          log.info(description.capitalize + " successful.")
-        }
-    }
-  private[sbt] def cached(storeFactory: CacheStoreFactory, doDoc: JavaDoc): JavaDoc =
-    new JavaDoc {
-      def run(
-          sources: List[VirtualFile],
-          classpath: List[VirtualFile],
-          converter: FileConverter,
-          outputDirectory: Path,
-          options: List[String],
-          incToolOptions: IncToolOptions,
-          log: Logger,
-          reporter: Reporter
-      ): Unit = {
-        val cp = classpath.map(converter.toPath).map(_.toFile)
-        val inputs = Inputs(
-          sources map { x =>
-            VHashFileInfo(x, x.contentHash)
-          },
-          filesInfoToList(lastModified(cp.toSet)),
-          classpath,
-          outputDirectory,
-          options
-        )
-        val cachedDoc = inputChanged(storeFactory make "inputs") { (inChanged, in: Inputs) =>
-          inputChanged(storeFactory make "output") { (outChanged, outputs: List[PlainFileInfo]) =>
-            if (inChanged || outChanged) {
-              if (Files.exists(outputDirectory)) {
-                sbt.io.IO.delete(outputDirectory.toFile)
-              }
-              Files.createDirectories(outputDirectory)
-              doDoc.run(
-                sources,
-                classpath,
-                converter,
-                outputDirectory,
-                options,
-                incToolOptions,
-                log,
-                reporter
-              )
-            } else log.debug("Doc uptodate: " + outputDirectory.toAbsolutePath.toString)
-          }
-        }
-        cachedDoc(inputs)(filesInfoToList(exists(outputDirectory.toFile.allPaths.get.toSet)))
+    (sources, cp, converter, outputDirectory, options, incToolOptions, log, reporter) => {
+      if (sources.isEmpty)
+        log.info(s"No sources available, skipping $description...")
+      else {
+        log.info(s"${description.capitalize} to ${outputDirectory.toAbsolutePath}...")
+        doDoc.run(sources, cp, converter, outputDirectory, options, incToolOptions, log, reporter)
+        log.info(s"${description.capitalize} successful.")
       }
+    }
+
+  private[sbt] def cached(storeFactory: CacheStoreFactory, doDoc: JavaDoc): JavaDoc =
+    (sources, classpath, converter, outDir, options, incToolOptions, log, reporter) => {
+      def go() = {
+        if (Files.exists(outDir))
+          IO.delete(outDir.toFile)
+        Files.createDirectories(outDir)
+        doDoc.run(sources, classpath, converter, outDir, options, incToolOptions, log, reporter)
+      }
+      val srcs = sources.map(x => VHashFileInfo(x, x.contentHash))
+      val cp0 = classpath.map(converter.toPath(_).toFile)
+      val cp = filesInfoToList(FileInfo.lastModified(cp0.toSet))
+      val inputs = Inputs(srcs, cp, classpath, outDir, options)
+      val outputs = filesInfoToList(FileInfo.exists(outDir.toFile.allPaths.get.toSet))
+      inputChanged(storeFactory.make("inputs")) { (inChanged, _: Inputs) =>
+        inputChanged(storeFactory.make("output")) { (outChanged, _: List[PlainFileInfo]) =>
+          if (inChanged || outChanged) go()
+          else log.debug(s"Doc uptodate: ${outDir.toAbsolutePath}")
+        }
+      }.apply(inputs)(outputs)
     }
 
   private[this] final case class VHashFileInfo(file: VirtualFile, contentHash: Long)
@@ -156,19 +94,17 @@ object Doc {
       outputDirectory: Path,
       options: List[String]
   )
+
   private[sbt] def filesInfoToList[A <: FileInfo](info: FilesInfo[A]): List[A] =
-    info.files.toList sortBy { x =>
-      x.file.getAbsolutePath
-    }
+    info.files.toList.sortBy(x => x.file.getAbsolutePath)
+
   private[sbt] val javaSourcesOnly: VirtualFile => Boolean = _.name.endsWith(".java")
 
   class JavadocGenerationFailed extends Exception
 
   trait JavaDoc {
 
-    /**
-     * @throws JavadocGenerationFailed when generating javadoc fails
-     */
+    /** @throws JavadocGenerationFailed when generating javadoc fails */
     def run(
         sources: List[VirtualFile],
         classpath: List[VirtualFile],
@@ -179,5 +115,6 @@ object Doc {
         log: Logger,
         reporter: Reporter
     ): Unit
+
   }
 }
