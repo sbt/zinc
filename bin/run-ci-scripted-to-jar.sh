bf5b135366b5f12564a4bf09c1057db80a6af20b
Dale Wijnand <dale.wijnand@gmail.com>
2020-06-16 22:15:49 +0100

    Remove unused bin scripts

--- bin/run-ci-scripted-to-jar.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-set -o nounset
-
-PROJECT_ROOT="zincRoot"
-sbt -Dfile.encoding=UTF-8 \
-  -J-XX:ReservedCodeCacheSize=512M \
-  -J-Xms1024M -J-Xmx2048M -J-server \
-  "zincScriptedJVM2_12/test:run --to-jar"
