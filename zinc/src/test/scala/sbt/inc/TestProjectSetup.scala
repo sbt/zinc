ca8e95eb0c7c834ca6563f8476156c42ae22321d
Dale Wijnand <dale.wijnand@gmail.com>
2020-07-01 22:31:48 +0100

    Cleanup TestProjectSetup

--- zinc/src/test/scala/sbt/inc/TestProjectSetup.scala
+++ zinc/src/test/scala/sbt/inc/TestProjectSetup.scala
@@ -12,18 +12,21 @@
 package sbt
 package inc
 
-import java.io.File
 import java.net.URLClassLoader
+import java.nio.charset.StandardCharsets
 import java.nio.file.{ Files, Path, Paths }
 import java.util.Optional
+
 import sbt.internal.inc._
+import sbt.internal.inc.JavaInterfaceUtil.EnrichOptional
 import sbt.internal.inc.classpath.ClassLoaderCache
 import sbt.internal.util.ManagedLogger
 import sbt.io.IO
 import sbt.io.syntax._
 import sbt.util.{ InterfaceUtil, Logger }
+
 import xsbti.{ FileConverter, VirtualFile }
-import xsbti.compile.{ ScalaInstance => _, _ }
+import xsbti.compile.{ ScalaInstance => XScalaInstance, _ }
 import xsbti.compile.FileAnalysisStore
 
 case class TestProjectSetup(
@@ -39,16 +42,13 @@ case class TestProjectSetup(
 
   private def fromResource(prefix: Path)(path: Path): File = {
     val fullPath = prefix.resolve(path).toString()
-    try {
-      Option(getClass.getClassLoader.getResource(fullPath))
-        .map(url => new File(url.toURI))
-        .getOrElse(throw new NoSuchElementException(s"Missing resource $fullPath"))
-    } catch {
-      case _: Throwable => sys.error(s"path = '$path' ($fullPath) not found")
+    getClass.getClassLoader.getResource(fullPath) match {
+      case null => sys.error(s"path = '$path' ($fullPath) not found")
+      case url  => new File(url.toURI)
     }
   }
 
-  private val localBoot = Paths.get(sys.props("user.home")).resolve(".sbt").resolve("boot")
+  private val localBoot = Paths.get(sys.props("user.home")).resolve(".sbt/boot")
   private val javaHome = Paths.get(sys.props("java.home"))
   private val sourcesPrefix = Paths.get("sources")
   private val binPrefix = Paths.get("bin")
@@ -58,59 +58,55 @@ case class TestProjectSetup(
     destinationRoot = baseLocation.resolve(destinationPath)
     sourceFile <- sourceFiles
   } yield {
-    val targetFile = destinationRoot.resolve(sourceFile).toFile
+    val targetFile = destinationRoot.resolve(sourceFile)
     val sourcePath = baseLocation.resolve(sourceFile)
     if (sourceFile.toString == "") {
       sys.error("unexpected blank sourceFile")
     }
     IO.copyFile(
-      if (sourcePath.isAbsolute && Files.exists(sourcePath)) sourcePath.toFile
+      if (Files.exists(sourcePath)) sourcePath.toFile
       else fromResource(sourcesPrefix)(sourceFile),
-      targetFile
+      targetFile.toFile
     )
 
-    targetFile.toPath
+    targetFile
   }
+
   val classpathBase = baseLocation.resolve("bin")
   val rootPaths = Map("BASE" -> baseLocation, "SBT_BOOT" -> localBoot, "JAVA_HOME" -> javaHome)
   val converter = new MappedFileConverter(rootPaths, true)
-  val allClasspath: Seq[VirtualFile] = (classPath map {
+
+  val allClasspath: Seq[Path] = classPath.map {
     case zippedClassesPath if zippedClassesPath.toString.endsWith(".zip") =>
-      val target = classpathBase.resolve(zippedClassesPath.toString.dropRight(4)).toFile
-      IO.unzip(fromResource(binPrefix)(zippedClassesPath), target)
-      target.toPath
+      val target = classpathBase.resolve(zippedClassesPath.toString.stripSuffix(".zip"))
+      IO.unzip(fromResource(binPrefix)(zippedClassesPath), target.toFile)
+      target
     case existingFile if Files.exists(existingFile) =>
       existingFile
     case jarPath =>
-      val newJar = classpathBase.resolve(jarPath).toFile
-      IO.copyFile(fromResource(binPrefix)(jarPath), newJar)
-      newJar.toPath
-  }).map(converter.toVirtualFile(_))
+      val newJar = classpathBase.resolve(jarPath)
+      IO.copyFile(fromResource(binPrefix)(jarPath), newJar.toFile)
+      newJar
+  }
 
   val defaultClassesDir: Path = baseLocation.resolve("classes")
-  val output: Path =
-    if (outputToJar) baseLocation.resolve("target").resolve("output.jar")
-    else defaultClassesDir
-  val earlyOutput: Path = baseLocation.resolve("target").resolve("early-output.jar")
+  val outputJar: Path = baseLocation.resolve("target/output.jar")
+  val earlyOutput: Path = baseLocation.resolve("target/early-output.jar")
+  val output: Path = if (outputToJar) outputJar else defaultClassesDir
 
   def defaultStoreLocation: Path = baseLocation.resolve("inc_data.zip")
   def defaultEarlyStoreLocation: Path = baseLocation.resolve("early_inc_data.zip")
 
-  def createCompiler(
-      scalaVersion: String,
-      si: xsbti.compile.ScalaInstance,
-      compilerBridge: Path,
-      log: ManagedLogger
-  ) =
+  def createCompiler(sv: String, si: XScalaInstance, compilerBridge: Path, log: ManagedLogger) = {
     CompilerSetup(
-      scalaVersion,
+      sv,
       si,
       compilerBridge,
       output,
       earlyOutput,
       baseLocation,
-      allSources.toVector map converter.toVirtualFile,
-      allClasspath,
+      allSources.toVector.map(converter.toVirtualFile(_)),
+      allClasspath.map(converter.toVirtualFile(_)),
       scalacOptions,
       IncOptions.of(),
       analysisForCp,
@@ -119,18 +115,18 @@ case class TestProjectSetup(
       converter,
       log
     )
+  }
 
   def update(source: Path)(change: String => String): Unit = {
-    import collection.JavaConverters._
     val sourceFile = baseLocation.resolve(source)
-    val text = Files.readAllLines(sourceFile).asScala.mkString("\n")
-    Files.write(sourceFile, Seq(change(text)).asJava)
+    val text = new String(Files.readAllBytes(sourceFile), StandardCharsets.UTF_8)
+    Files.write(sourceFile, change(text).getBytes(StandardCharsets.UTF_8))
     ()
   }
 
   def dependsOnJarFrom(other: TestProjectSetup): TestProjectSetup = {
     val sources = other.defaultClassesDir.toFile ** "*.class"
-    val mapping = sources.get.map { file =>
+    val mapping = sources.get().map { file =>
       file -> other.defaultClassesDir.relativize(file.toPath).toString
     }
     val dest = baseLocation.resolve("bin").resolve(s"${other.baseLocation.getFileName}.jar")
@@ -144,16 +140,13 @@ case class TestProjectSetup(
 }
 
 object TestProjectSetup {
-  def simple(baseLocation: Path, classes: Seq[String]): TestProjectSetup =
-    TestProjectSetup(
-      baseLocation,
-      Map(Paths.get("src") -> classes.map(path => Paths.get(path))),
-      Nil,
-      Map.empty
-    )
+  def simple(baseLocation: Path, classes: Seq[String]): TestProjectSetup = {
+    val sources = Map(Paths.get("src") -> classes.map(path => Paths.get(path)))
+    TestProjectSetup(baseLocation, sources, Nil, Map.empty)
+  }
 
-  def scalaCompiler(instance: xsbti.compile.ScalaInstance, bridgeJar: Path): AnalyzingCompiler = {
-    val bridgeProvider = ZincUtil.constantBridgeProvider(instance, bridgeJar.toFile)
+  def scalaCompiler(instance: XScalaInstance, bridgeJar: Path): AnalyzingCompiler = {
+    val bridgeProvider = ZincUtil.constantBridgeProvider(instance, bridgeJar)
     val classpath = ClasspathOptionsUtil.boot
     val cache = Some(new ClassLoaderCache(new URLClassLoader(Array())))
     new AnalyzingCompiler(instance, bridgeProvider, classpath, _ => (), cache)
@@ -161,7 +154,7 @@ object TestProjectSetup {
 
   case class CompilerSetup(
       scalaVersion: String,
-      si: xsbti.compile.ScalaInstance,
+      si: XScalaInstance,
       compilerBridge: Path,
       output: Path,
       earlyOutput: Path,
@@ -221,13 +214,15 @@ object TestProjectSetup {
       Some(FileAnalysisStore.getDefault(earlyAnalysisStoreLocation.toFile)),
       extra
     )
+
     val prev = compiler.emptyPreviousResult
+
     val cp = Vector(converter.toVirtualFile(output)) ++
-      (si.allJars map { x =>
-        converter.toVirtualFile(x.toPath)
-      }) ++
+      si.allJars.map(x => converter.toVirtualFile(x.toPath)) ++
       classpath.toVector
+
     val stamper = Stamps.timeWrapLibraryStamps(converter)
+
     val in = compiler.inputs(
       cp.toArray,
       sources.toArray,
@@ -255,7 +250,6 @@ object TestProjectSetup {
         store: AnalysisStore = FileAnalysisStore.getDefault(analysisStoreLocation.toFile),
         newInputs: Inputs => Inputs = identity
     ): CompileResult = {
-      import JavaInterfaceUtil.EnrichOptional
       val previousResult = store.get().toOption match {
         case Some(analysisContents) =>
           val prevAnalysis = analysisContents.getAnalysis
@@ -272,9 +266,8 @@ object TestProjectSetup {
       store.set(AnalysisContents.create(newResult.analysis(), newResult.setup()))
       newResult
     }
-    def close(): Unit = {
-      sc.classLoaderCache.foreach(_.close())
-    }
+
+    def close(): Unit = sc.classLoaderCache.foreach(_.close())
   }
 
   case class MockedLookup(am: VirtualFile => Optional[CompileAnalysis])
