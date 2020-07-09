c0f20fcfbead2d134b89386889ebf1a16fd9236e
Dale Wijnand <dale.wijnand@gmail.com>
2020-07-09 12:16:50 +0100

    Cleanup OutputSpec & less spam

--- zinc/src/test/scala/sbt/inc/OutputSpec.scala
+++ zinc/src/test/scala/sbt/inc/OutputSpec.scala
@@ -11,59 +11,32 @@
 
 package sbt.inc
 
+import java.io.File
 import java.nio.file.Files
-import sbt.io.IO
-import sbt.internal.inc.JavaInterfaceUtil._
-import sbt.internal.inc._
-import xsbti.compile.IncOptions
+import sbt.io.IO.{ withTemporaryDirectory => withTmpDir }
 
 class OutputSpec extends BaseCompilerSpec {
-  override val logLevel = sbt.util.Level.Debug
-  def isJava8: Boolean = sys.props("java.specification.version") == "1.8"
+  //override val logLevel = sbt.util.Level.Debug
+  behavior.of("incremental compiler")
 
-  "incremental compiler" should "compile directly to JAR" in {
-    IO.withTemporaryDirectory { tempDir =>
-      val projectSetup = ProjectSetup
-        .simple(tempDir.toPath, Seq(SourceFiles.Good))
-        .copy(outputToJar = true)
-      val compilerSetup = projectSetup
-        .createCompiler()
-        .copy(incOptions = IncOptions.of().withAllowMachinePath(false))
-      val result = compilerSetup.doCompile()
-      val expectedOuts = List(projectSetup.output)
-      expectedOuts foreach { f =>
-        assert(Files.exists(f), s"$f does not exist.")
-      }
-      val a = result.analysis match { case a: Analysis => a }
-      assert(a.stamps.allSources.nonEmpty)
-    }
+  it should "compile directly to JAR" in withTmpDir { baseDir =>
+    val compiler = mkCompiler(baseDir, Seq(SourceFiles.Good))
+    val result = compiler.doCompile()
+    assert(Files.exists(compiler.output), s"${compiler.output} does not exist.")
+    assert(!result.analysis.readStamps.getAllSourceStamps.keySet.isEmpty)
   }
 
-  it should "not compile anything if source has not changed" in {
-    if (isJava8)
-      IO.withTemporaryDirectory { tempDir =>
-        val projectSetup =
-          ProjectSetup
-            .simple(tempDir.toPath, Seq(SourceFiles.Good, SourceFiles.Foo))
-            .copy(outputToJar = true)
-        val compilerSetup = projectSetup
-          .createCompiler()
-          .copy(incOptions = IncOptions.of().withAllowMachinePath(false))
-        val result = compilerSetup.doCompileWithStore()
-        log.info("==========================================")
-        log.info("begin second compilation")
-        val prev = compilerSetup.compiler.previousResult(result)
-        val analysis: Analysis = prev.analysis.toOption.get match {
-          case a: Analysis => a
-        }
-        log.info(s"prev = $analysis")
-        log.info(s"prev.stamps = ${analysis.stamps}")
-        val result2 = compilerSetup.doCompileWithStore()
+  it should "not compile anything if source has not changed" in withTmpDir { baseDir =>
+    val compiler = mkCompiler(baseDir, Seq(SourceFiles.Good, SourceFiles.Foo))
+    val result = compiler.doCompileWithStore()
+    assert(!result.analysis.readStamps.getAllSourceStamps.keySet.isEmpty)
 
-        assert(!result2.hasModified)
-      } else {
-      // This test fails on zincJVM2_13 + JDK 11
-      ()
-    }
+    val result2 = compiler.doCompileWithStore()
+
+    if (sys.props("java.specification.version") == "1.8") // This test fails on Scala 2.13 + JDK 11
+      assert(!result2.hasModified || sys.props("java.specification.version") != "1.8")
   }
+
+  def mkCompiler(baseDir: File, classes: Seq[String]) =
+    ProjectSetup.simple(baseDir.toPath, classes).copy(outputToJar = true).createCompiler()
 }
