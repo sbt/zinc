sbt/zinc

commit 9310afe6cf52d7feed83d25eda622d0e9256a158

Let the build tools configure -Ypickle-write flag

internal/zinc-core/src/main/scala/sbt/internal/inc/Incremental.scala
@@ -339,15 +339,20 @@ object Incremental {
     val (initialInvClasses, initialInvSources0) =
       incremental.invalidateInitial(previous.relations, initialChanges)
 
+    val isPickleWrite = currentSetup.options.scalacOptions.contains("-Ypickle-write")
+    if (!isPickleWrite && options.pipelining) {
+      log.warn(s"-Ypickle-write should be included into scalacOptions if pipelining is enabled")
+    }
     // If there's any compilation at all, invalidate all java sources too, so we have access to their type information.
     val javaSources: Set[VirtualFileRef] = sources
       .filter(_.name.endsWith(".java"))
       .map(_.asInstanceOf[VirtualFileRef])
     val isPickleJava = currentSetup.options.scalacOptions.contains("-Ypickle-java")
-    assert(
-      javaSources.isEmpty || !options.pipelining || isPickleJava,
-      s"-Ypickle-java must be included into scalacOptions if pipelining is enabled with Java sources"
-    )
+    if (javaSources.nonEmpty && options.pipelining && !isPickleJava) {
+      log.warn(
+        s"-Ypickle-java should be included into scalacOptions if pipelining is enabled with Java sources"
+      )
+    }
     val initialInvSources =
       if (initialInvSources0.nonEmpty) initialInvSources0 ++ javaSources
       else Set.empty[VirtualFileRef]

zinc/src/main/scala/sbt/internal/inc/MixedAnalyzingCompiler.scala
@@ -143,11 +143,10 @@ final class MixedAnalyzingCompiler(
     val isPickleJava = config.currentSetup.order == Mixed && config.incOptions.pipelining && javaSrcs.nonEmpty
 
     val earlyOut = config.earlyOutput.flatMap(_.getSingleOutputAsPath.toOption)
-    val pickleWrite = earlyOut.toList.flatMap { out =>
-      val sbv = scalac.scalaInstance.version.take(4)
-      if (out.toString.endsWith(".jar") && !Files.exists(out))
+    earlyOut foreach { out =>
+      if (out.toString.endsWith(".jar") && !Files.exists(out)) {
         scala.reflect.io.RootPath(out, writable = true).close() // creates an empty jar
-      List("-Ypickle-write", out.toString).filter(_ => sbv == "2.12" || sbv == "2.13")
+      }
     }
 
     // Compile Scala sources.
@@ -162,7 +161,7 @@ final class MixedAnalyzingCompiler(
             config.converter.toVirtualFile(x.toAbsolutePath)
           }) ++ absClasspath
           val arguments =
-            cArgs.makeArguments(Nil, cp, config.currentSetup.options.scalacOptions ++ pickleWrite)
+            cArgs.makeArguments(Nil, cp, config.currentSetup.options.scalacOptions)
           timed("Scala compilation", log) {
             config.compiler.compile(
               sources.toArray,

internal/zinc-scripted/src/test/scala/sbt/internal/inc/IncHandler.scala
@@ -761,9 +761,10 @@ case class ProjectStructure(
     }
     val scalacOptions: List[String] =
       (Option(map.get("scalac.options")) match {
-        case Some(x)                    => List(x)
-        case _ if incOptions.pipelining => List("-Ypickle-java")
-        case _                          => Nil
+        case Some(x) => List(x)
+        case _ if incOptions.pipelining =>
+          List("-Ypickle-java", "-Ypickle-write", earlyOutput.toString)
+        case _ => Nil
       }).flatMap(_.toString.split(" +").toList)
     (incOptions, scalacOptions.toArray)
   }

zinc/src/test/scala/sbt/inc/TestProjectSetup.scala
@@ -229,7 +229,8 @@ object TestProjectSetup {
       sources.toArray,
       output,
       Some(earlyOutput),
-      scalacOptions = (Vector("-Ypickle-java") ++ scalacOptions).toArray,
+      scalacOptions =
+        (Vector("-Ypickle-java", "-Ypickle-write", earlyOutput.toString) ++ scalacOptions).toArray,
       javacOptions = Array(),
       maxErrors,
       sourcePositionMappers = Array(),

commit af29a4aedc10e1755833ce33b274d58fbd12ce6d

Set -Ypickle-write when required.
Reverts the regression introduced in 9310afe6cf52d7feed83d25eda622d0e9256a158.

internal/zinc-core/src/main/scala/sbt/internal/inc/Incremental.scala
@@ -339,20 +339,15 @@ object Incremental {
     val (initialInvClasses, initialInvSources0) =
       incremental.invalidateInitial(previous.relations, initialChanges)
 
-    val isPickleWrite = currentSetup.options.scalacOptions.contains("-Ypickle-write")
-    if (!isPickleWrite && options.pipelining) {
-      log.warn(s"-Ypickle-write should be included into scalacOptions if pipelining is enabled")
-    }
     // If there's any compilation at all, invalidate all java sources too, so we have access to their type information.
     val javaSources: Set[VirtualFileRef] = sources
       .filter(_.name.endsWith(".java"))
       .map(_.asInstanceOf[VirtualFileRef])
     val isPickleJava = currentSetup.options.scalacOptions.contains("-Ypickle-java")
-    if (javaSources.nonEmpty && options.pipelining && !isPickleJava) {
-      log.warn(
-        s"-Ypickle-java should be included into scalacOptions if pipelining is enabled with Java sources"
-      )
-    }
+    assert(
+      javaSources.isEmpty || !options.pipelining || isPickleJava,
+      s"-Ypickle-java must be included into scalacOptions if pipelining is enabled with Java sources"
+    )
     val initialInvSources =
       if (initialInvSources0.nonEmpty) initialInvSources0 ++ javaSources
       else Set.empty[VirtualFileRef]

zinc/src/main/scala/sbt/internal/inc/MixedAnalyzingCompiler.scala
@@ -143,10 +143,11 @@ final class MixedAnalyzingCompiler(
     val isPickleJava = config.currentSetup.order == Mixed && config.incOptions.pipelining && javaSrcs.nonEmpty
 
     val earlyOut = config.earlyOutput.flatMap(_.getSingleOutputAsPath.toOption)
-    earlyOut foreach { out =>
-      if (out.toString.endsWith(".jar") && !Files.exists(out)) {
+    val pickleWrite = earlyOut.toList.flatMap { out =>
+      val sbv = scalac.scalaInstance.version.take(4)
+      if (out.toString.endsWith(".jar") && !Files.exists(out))
         scala.reflect.io.RootPath(out, writable = true).close() // creates an empty jar
-      }
+      List("-Ypickle-write", out.toString).filter(_ => sbv == "2.12" || sbv == "2.13")
     }
 
     // Compile Scala sources.
@@ -161,7 +162,7 @@ final class MixedAnalyzingCompiler(
             x.toAbsolutePath
           }) ++ absClasspath.map(config.converter.toPath)
           val arguments =
-            cArgs.makeArguments(Nil, cp, config.currentSetup.options.scalacOptions)
+            cArgs.makeArguments(Nil, cp, config.currentSetup.options.scalacOptions ++ pickleWrite)
           timed("Scala compilation", log) {
             config.compiler.compile(
               sources.toArray,

internal/zinc-scripted/src/test/scala/sbt/internal/inc/IncHandler.scala
@@ -761,10 +761,9 @@ case class ProjectStructure(
     }
     val scalacOptions: List[String] =
       (Option(map.get("scalac.options")) match {
-        case Some(x) => List(x)
-        case _ if incOptions.pipelining =>
-          List("-Ypickle-java", "-Ypickle-write", earlyOutput.toString)
-        case _ => Nil
+        case Some(x)                    => List(x)
+        case _ if incOptions.pipelining => List("-Ypickle-java")
+        case _                          => Nil
       }).flatMap(_.toString.split(" +").toList)
     (incOptions, scalacOptions.toArray)
   }

zinc/src/test/scala/sbt/inc/TestProjectSetup.scala
@@ -229,8 +229,7 @@ object TestProjectSetup {
       sources.toArray,
       output,
       Some(earlyOutput),
-      scalacOptions =
-        (Vector("-Ypickle-java", "-Ypickle-write", earlyOutput.toString) ++ scalacOptions).toArray,
+      scalacOptions = (Vector("-Ypickle-java") ++ scalacOptions).toArray,
       javacOptions = Array(),
       maxErrors,
       sourcePositionMappers = Array(),