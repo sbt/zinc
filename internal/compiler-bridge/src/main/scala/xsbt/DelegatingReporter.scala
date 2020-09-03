sbt/zinc

commit 8b2db7a792f3dbf47d31d6e543b353b4e1a42834

Convert VirtualFile to PlainFile in the compiler bridge

internal/compiler-bridge/src/main/scala/xsbt/DelegatingReporter.scala
@@ -89,7 +89,9 @@ private object DelegatingReporter {
 
     def makePosition(pos: Position): xsbti.Position = {
       val src = pos.source
-      val sourcePath = src.file.path
+      val sourcePath = src.file match {
+        case VirtualFileWrap(virtualFile) => virtualFile.id
+      }
       val sourceFile = new File(src.file.path)
       val line = pos.line
       val lineContent = pos.lineContent.stripLineEnd

internal/compiler-bridge/src/main/scala/xsbt/CompilerInterface.scala
@@ -154,7 +154,7 @@ private final class CachedCompiler0(args: Array[String], output: Output, initial
       compiler.set(callback, underlyingReporter)
       val run = new compiler.ZincRun(compileProgress)
 
-      val wrappedFiles = sources.map(new VirtualFileWrap(_))
+      val wrappedFiles = sources.map(VirtualFileWrap(_))
       val sortedSourceFiles: List[AbstractFile] =
         wrappedFiles.sortWith(_.underlying.id < _.underlying.id)
       run.compileFiles(sortedSourceFiles)

internal/compiler-bridge/src/main/scala/xsbt/ScaladocInterface.scala
@@ -64,7 +64,7 @@ private class Runner(
       def document(ignore: Seq[String]): Unit = {
         import compiler._
         val run = new Run
-        val wrappedFiles = sources.toList.map(new VirtualFileWrap(_))
+        val wrappedFiles = sources.toList.map(VirtualFileWrap(_))
         val sortedSourceFiles: List[AbstractFile] =
           wrappedFiles.sortWith(_.underlying.id < _.underlying.id)
         run.compileFiles(sortedSourceFiles)

internal/compiler-bridge/src/main/scala/xsbt/VirtualFileWrap.scala
@@ -11,81 +11,44 @@
 
 package xsbt
 
-import xsbti.VirtualFile
-import scala.reflect.io.AbstractFile
-import java.io.{ File, InputStream, OutputStream }
+import java.io.{ InputStream, OutputStream }
+import xsbti.{ PathBasedFile, VirtualFile }
+import scala.reflect.io.{ AbstractFile, Path, PlainFile }
 
-final class VirtualFileWrap(val underlying: VirtualFile) extends AbstractFile {
-  // scala.tools.nsc.CompilationUnits$CompilationUnit.<init>(CompilationUnits.scala:161)
-  override def name: String = underlying.name
+private trait VirtualFileWrap extends AbstractFile {
+  def underlying: VirtualFile
+}
 
-  // scala.tools.nsc.Global$Run.addUnit(Global.scala:1353)
-  override def path: String = underlying.id
+private final class XsbtPlainFile(val underlying: PathBasedFile)
+    extends PlainFile(Path(underlying.toPath.toFile))
+    with VirtualFileWrap
 
-  // at scala.tools.nsc.io.SourceReader.read(SourceReader.scala:62)
-  override def input: InputStream = underlying.input
+private final class XsbtVirtualFile private[xsbt] (val underlying: VirtualFile)
+    extends reflect.io.VirtualFile(underlying.name, underlying.id)
+    with VirtualFileWrap {
 
-  override def absolute: AbstractFile = {
-    ???
-    // abstractFile.absolute
-  }
+  // fill the in-memory reflect.io.VirtualFile with the content of the underlying xsbti.VirtualFile
+  copyTo(underlying.input(), output)
 
-  // used only by Scala 2.10
-  // https://github.com/scala/scala/blob/v2.10.7/src/compiler/scala/tools/nsc/Global.scala#L1726
-  override def container: AbstractFile = {
-    new AbstractFile {
-      override def name: String = "temp"
-      def absolute: AbstractFile = ???
-      def container: AbstractFile = ???
-      def create(): Unit = ???
-      def delete(): Unit = ???
-      def file: File = ???
-      def input: InputStream = ???
-      def isDirectory: Boolean = true
-      def iterator: Iterator[AbstractFile] = ???
-      def lastModified: Long = ???
-      def lookupName(name: String, directory: Boolean): AbstractFile = ???
-      def lookupNameUnchecked(name: String, directory: Boolean): AbstractFile = ???
-      def output: OutputStream = ???
-      def path: String = ???
+  private def copyTo(input: InputStream, output: OutputStream): Unit = {
+    while (input.available > 0) {
+      val content = new Array[Byte](input.available)
+      input.read(content)
+      output.write(content)
     }
+    input.close()
+    output.close()
+  }
+}
+
+private object VirtualFileWrap {
+  def apply(virtualFile: VirtualFile): VirtualFileWrap = virtualFile match {
+    case file: PathBasedFile => new XsbtPlainFile(file)
+    case _                   => new XsbtVirtualFile(virtualFile)
   }
 
-  override def file: File = {
-    null
-  }
-
-  override def create(): Unit = {
-    ???
-    // abstractFile.create()
-  }
-  override def delete(): Unit = {
-    ???
-    /// abstractFile.delete()
-  }
-  override def isDirectory: Boolean = {
-    ???
-    // abstractFile.isDirectory
-  }
-  override def lastModified: Long = {
-    ???
-    // abstractFile.lastModified
-  }
-
-  override def output: OutputStream = {
-    ???
-    // abstractFile.output
-  }
-  override def iterator: Iterator[AbstractFile] = {
-    ???
-    // abstractFile.iterator
-  }
-  override def lookupName(name: String, directory: Boolean): AbstractFile = {
-    ???
-    // abstractFile.lookupName(name, directory)
-  }
-  override def lookupNameUnchecked(name: String, directory: Boolean): AbstractFile = {
-    ???
-    // abstractFile.lookupNameUnchecked(name, directory)
+  def unapply(abstractFile: AbstractFile): Option[VirtualFile] = abstractFile match {
+    case wrapper: VirtualFileWrap => Some(wrapper.underlying)
+    case _                        => None
   }
 }

commit a05e58ab8511b6171346d0b5fb98c76fe8722e41

Fix match error in DelegatingReporter.scala
8b2db7a792f3dbf47d31d6e543b353b4e1a42834 introduced a regression because
it is not always the case that the source passed in to
DelegateReporter.makePosition is an instance of AbstractZincFile.
53bc41acc7d878617f0d8b6f9400d606e8191637 partially fixed this by
handling PlainFile but the proper fix is to handle AbstractFile which is
what was done prior to 8b2db7a792f3dbf47d31d6e543b353b4e1a42834

This was detected by running the community build in the shapeless
project.

internal/compiler-bridge/src/main/scala/xsbt/DelegatingReporter.scala
@@ -17,7 +17,7 @@ import java.util.Optional
 import scala.reflect.internal.util.{ FakePos, NoPosition, Position }
 // Left for compatibility
 import Compat._
-import scala.reflect.io.PlainFile
+import scala.reflect.io.AbstractFile
 
 private object DelegatingReporter {
   def apply(settings: scala.tools.nsc.Settings, delegate: xsbti.Reporter): DelegatingReporter =
@@ -92,7 +92,7 @@ private object DelegatingReporter {
       val src = pos.source
       val sourcePath = src.file match {
         case AbstractZincFile(virtualFile) => virtualFile.id
-        case f: PlainFile                  => f.file.toString
+        case af: AbstractFile              => af.path
       }
       val sourceFile = new File(src.file.path)
       val line = pos.line