7a5d6c4600040286e75f69fc966d9b7aee6974c4
Eugene Yokota <eed3si9n@gmail.com>
2021-09-19 14:18:55 -0400

    Update to Scala 2.12.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,13 +7,13 @@ object Dependencies {
 
   val scala210 = "2.10.7"
   val scala211 = "2.11.12"
-  val scala212 = "2.12.14"
+  val scala212 = "2.12.15"
   val scala213 = "2.13.6"
   val defaultScalaVersion = scala212
   val allScalaVersions = Seq(defaultScalaVersion, scala210, scala211, scala213)
   val scala212_213 = Seq(defaultScalaVersion, scala213)
 
-  private val ioVersion = nightlyVersion.getOrElse("1.5.0")
+  private val ioVersion = nightlyVersion.getOrElse("1.6.0-M1")
   private val utilVersion = nightlyVersion.getOrElse("1.5.0")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
