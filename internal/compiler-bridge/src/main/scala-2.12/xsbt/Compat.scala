05d188981466ebbe0812573266a0e7bdd4c837bc
Eugene Yokota <eed3si9n@gmail.com>
2021-12-15 02:50:19 -0500

    Revert "Make accessing OriginalTree noop on Scala 2.12.0-2"
    This reverts commit 8c5e395c8c1d704501b3e5a27e91b3716f288792.

--- internal/compiler-bridge/src/main/scala-2.12/xsbt/Compat.scala
+++ internal/compiler-bridge/src/main/scala-2.12/xsbt/Compat.scala
@@ -22,7 +22,10 @@ abstract class Compat {
 
   /** If given tree contains object tree attachment calls func on tree from attachment. */
   protected def processOriginalTreeAttachment(in: Tree)(func: Tree => Unit): Unit = {
-    Compat.OriginalTreeTraverser.Instance.traverseOriginal(in)(func)
+    import analyzer._
+    in.attachments.get[OriginalTreeAttachment].foreach { a =>
+      func(a.original)
+    }
   }
 }
 object Compat {
@@ -31,32 +34,6 @@ object Compat {
 
   // IMain in 2.13 accepts ReplReporter
   def replReporter(settings: Settings, writer: PrintWriter) = writer
-
-  sealed abstract class OriginalTreeTraverser private {
-    def traverseOriginal[T <: Global#Tree](t: T)(f: T => Unit): Unit
-  }
-
-  object OriginalTreeTraverser {
-    private[this] val cls = try {
-      Class.forName("scala.tools.nsc.typechecker.StdAttachments$OriginalTreeAttachment")
-    } catch { case _: Throwable => null }
-
-    private object Reflective extends OriginalTreeTraverser {
-      private[this] val ct = scala.reflect.ClassTag(cls)
-      private[this] val meth = cls.getMethod("original")
-      def traverseOriginal[T <: Global#Tree](t: T)(f: T => Unit): Unit =
-        t.attachments.get(ct) match {
-          case Some(attachment) => f(meth.invoke(attachment).asInstanceOf[T])
-          case None             =>
-        }
-    }
-
-    private object NoOp extends OriginalTreeTraverser {
-      def traverseOriginal[T <: Global#Tree](t: T)(f: T => Unit): Unit = ()
-    }
-
-    val Instance = if (cls == null) NoOp else Reflective
-  }
 }
 
 /** Defines compatibility utils for [[ZincCompiler]]. */
