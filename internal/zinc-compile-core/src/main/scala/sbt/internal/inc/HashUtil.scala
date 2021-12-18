0189fa36169efd5c7fcf604f412621d4a7b7973b
Eugene Yokota <eed3si9n@gmail.com>
2021-12-18 02:49:52 -0500

    Fixes hashing large files
    Fixes https://github.com/sbt/zinc/issues/1015

--- internal/zinc-compile-core/src/main/scala/sbt/internal/inc/HashUtil.scala
+++ internal/zinc-compile-core/src/main/scala/sbt/internal/inc/HashUtil.scala
@@ -13,13 +13,23 @@ package sbt
 package internal
 package inc
 
-import java.nio.file.{ Files, Path }
+import java.nio.channels.FileChannel
+import java.nio.file.{ Path, StandardOpenOption }
+import java.nio.ByteBuffer
 import net.openhft.hashing.LongHashFunction
 
 object HashUtil {
   def farmHash(bytes: Array[Byte]): Long =
     LongHashFunction.farmNa().hashBytes(bytes)
 
+  def farmHash(buffer: ByteBuffer): Long =
+    LongHashFunction.farmNa().hashBytes(buffer)
+
   def farmHash(path: Path): Long =
-    farmHash(Files.readAllBytes(path))
+    farmHash(mappedBuffer(path))
+
+  private def mappedBuffer(path: Path): ByteBuffer = {
+    val fileChannel = FileChannel.open(path, StandardOpenOption.READ)
+    fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size())
+  }
 }
