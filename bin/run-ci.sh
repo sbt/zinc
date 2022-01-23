72f3ed9efb13852a0259e4b7d7cf7071a838fb47
Eugene Yokota <eed3si9n@gmail.com>
2022-01-23 03:01:48 -0500

    Turn off mima

--- bin/run-ci.sh
+++ bin/run-ci.sh
@@ -5,10 +5,8 @@ set -o nounset
 sbt -Dfile.encoding=UTF-8 \
   -J-XX:ReservedCodeCacheSize=512M \
   -J-Xms1024M -J-Xmx2048M -J-server \
-  mimaReportBinaryIssues \
   scalafmtCheckAll \
   scalafmtSbtCheck \
-  whitesourceCheckPolicies \
   Test/compile \
   doc \
   crossTestBridges \
