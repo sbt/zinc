472f215f237f4654949149fd4d1150174dbb6d9b
Seth Tisue <seth@tisue.net>
2022-02-03 17:36:39 -0800

    centralize & modernize JVM settings & increase heap size
    this repo uses sbt-projectmatrix for crossbuilding which means
    it can be very heap-hungry if you do anything across all subprojects.
    I think it's reasonable to go to the 2G heap size for local
    development, not just on CI like before.
    
    I've also removed `-Xms` as I doubt it's helping anything
    
    I've also removed `-server` (this is surely left over from some ancient
    config, it's the default these days)
    
    and I've removed the CodeCacheSize setting which I'm less sure about
    but I suspect is also just an ancient holdo0ver

--- bin/run-ci.sh
+++ bin/run-ci.sh
@@ -3,8 +3,6 @@ set -eu
 set -o nounset
 
 sbt -Dfile.encoding=UTF-8 \
-  -J-XX:ReservedCodeCacheSize=512M \
-  -J-Xms1024M -J-Xmx2048M -J-server \
   mimaReportBinaryIssues \
   scalafmtCheckAll \
   scalafmtSbtCheck \
