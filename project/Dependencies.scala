87d562a27f99406fb86797fa3d4788af65be17de
Eugene Yokota <eed3si9n@gmail.com>
2022-07-10 05:23:43 -0400

    util 1.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,8 +13,8 @@ object Dependencies {
   val allScalaVersions = Seq(defaultScalaVersion, scala210, scala211, scala213)
   val scala212_213 = Seq(defaultScalaVersion, scala213)
 
-  private val ioVersion = nightlyVersion.getOrElse("1.6.0")
-  private val utilVersion = nightlyVersion.getOrElse("1.7.0-RC2")
+  private val ioVersion = nightlyVersion.getOrElse("1.7.0")
+  private val utilVersion = nightlyVersion.getOrElse("1.7.0")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
