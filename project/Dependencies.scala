d487a60083cb7b901aa4ca641b0db4eb4598e20c
Scala Steward <scala_steward@virtuslab.com>
2022-09-17 22:53:52 +0000

    Update scalacheck to 1.17.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -72,7 +72,7 @@ object Dependencies {
 
   val parserCombinator = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
   val sbinary = "org.scala-sbt" %% "sbinary" % "0.5.1"
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.16.0"
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.17.0"
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.13"
   val verify = "com.eed3si9n.verify" %% "verify" % "2.0.1"
   val sjsonnew = Def.setting {
