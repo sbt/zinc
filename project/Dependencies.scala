956dd0933db88ba540fceaa4f0985a702bbbeec7
Eugene Yokota <eed3si9n@gmail.com>
2022-10-18 00:11:00 -0400

    util 2.0.0-alpha3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val scala3_only = Seq(scala3)
 
   private val ioVersion = nightlyVersion.getOrElse("1.6.0")
-  private val utilVersion = nightlyVersion.getOrElse("2.0.0-alpha2")
+  private val utilVersion = nightlyVersion.getOrElse("2.0.0-alpha3")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
