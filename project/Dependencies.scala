29dcc40f714bdff9bd5935eca9f7be2cbe6fa6c7
Scala Steward <scala_steward@virtuslab.com>
2022-10-20 01:48:58 +0000

    Update launcher-interface to 1.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
   private val utilInterface = "org.scala-sbt" % "util-interface" % utilVersion
   private val utilScripted = "org.scala-sbt" %% "util-scripted" % utilVersion
 
-  val launcherInterface = "org.scala-sbt" % "launcher-interface" % "1.3.3"
+  val launcherInterface = "org.scala-sbt" % "launcher-interface" % "1.4.1"
 
   def getSbtModulePath(key: String, name: String) = {
     val localProps = new java.util.Properties()
