0f20f1418f1a499275a8af8ce2476c8fa21572f1
Scala Steward <scala_steward@virtuslab.com>
2022-10-20 01:50:03 +0000

    Update scalatest to 3.2.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,7 +73,7 @@ object Dependencies {
   val parserCombinator = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
   val sbinary = "org.scala-sbt" %% "sbinary" % "0.5.1"
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.17.0"
-  val scalatest = "org.scalatest" %% "scalatest" % "3.2.13"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.14"
   val verify = "com.eed3si9n.verify" %% "verify" % "2.0.1"
   val sjsonnew = Def.setting {
     "com.eed3si9n" %% "sjson-new-core" % contrabandSjsonNewVersion.value
