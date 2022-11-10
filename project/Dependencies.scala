ee9212b46e2a1c2396efa8871775ff41bd891aab
Scala Steward <scala_steward@virtuslab.com>
2022-11-10 20:38:59 +0000

    Revert commit(s) 122da89e8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val scala210 = "2.10.7"
   val scala211 = "2.11.12"
   val scala212 = "2.12.16"
-  val scala213 = "2.13.10"
+  val scala213 = "2.13.8"
   val defaultScalaVersion = scala212
   val allScalaVersions = Seq(defaultScalaVersion, scala210, scala211, scala213)
   val scala212_213 = Seq(defaultScalaVersion, scala213)
