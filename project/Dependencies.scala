e3eaa0c129f5f892ca8d9167e6af38389c7f06ab
Scala Steward <scala_steward@virtuslab.com>
2022-11-10 20:39:09 +0000

    Update scala-compiler, scala-library to 2.13.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val scala210 = "2.10.7"
   val scala211 = "2.11.12"
   val scala212 = "2.12.17"
-  val scala213 = "2.13.8"
+  val scala213 = "2.13.10"
   val defaultScalaVersion = scala212
   val allScalaVersions = Seq(defaultScalaVersion, scala210, scala211, scala213)
   val scala212_213 = Seq(defaultScalaVersion, scala213)
