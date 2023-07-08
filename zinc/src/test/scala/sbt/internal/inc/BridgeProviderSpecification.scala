87eebcedea68b77619f3f91e7b04af8964f2901a
Eugene Yokota <eed3si9n@gmail.com>
2023-07-08 15:59:55 -0400

    Provide an improved DelegatingReporter
    **Problem**
    We need to surface the code action from Scala 2.13.x.
    
    **Solution**
    This adds an alternative DelegatingReporter implementation as a
    resource. The ZincComponentManager can then substitute the source
    for 2.13.12 onwards.

--- zinc/src/test/scala/sbt/internal/inc/BridgeProviderSpecification.scala
+++ zinc/src/test/scala/sbt/internal/inc/BridgeProviderSpecification.scala
@@ -12,20 +12,48 @@
 package sbt.internal.inc
 
 import java.nio.file.Path
-import sbt.inc.{ ScalaBridge, ConstantBridgeProvider }
+import sbt.inc.{ CompilerSetup, ScalaBridge, ConstantBridgeProvider, ProjectSetup }
 import sbt.util.Logger
-import xsbti.compile.CompilerBridgeProvider
+import xsbti.compile.{ CompilerBridgeProvider, IncOptions }
+
+trait BridgeProviderSpec extends verify.BasicTestSuite with BridgeProviderTestkit
 
 class BridgeProviderSpecification extends UnitSpec with BridgeProviderTestkit {}
 
 trait BridgeProviderTestkit extends AbstractBridgeProviderTestkit {
+  def incOptions: IncOptions = IncOptions.of().withPipelining(true)
+
+  implicit class ProjectSetupOps(setup: ProjectSetup) {
+    import sbt.internal.inc.ZincBuildInfo._
+
+    def createCompiler(): CompilerSetup =
+      createCompiler(scalaVersion213, incOptions)
+
+    def createCompiler(scalaVersion: String): CompilerSetup =
+      createCompiler(scalaVersion, incOptions)
+
+    private def createCompiler(sv: String, incOptions: IncOptions): CompilerSetup = {
+      val si = scalaInstance(sv, setup.baseDir, Logger.Null)
+      val bridge = getCompilerBridge(setup.baseDir, Logger.Null, sv)
+      setup.createCompiler(sv, si, bridge, incOptions, log)
+    }
+  }
+
   lazy val bridges: List[ScalaBridge] = {
     import sbt.internal.inc.ZincBuildInfo._
     val compilerBridge210 = ScalaBridge(scalaVersion210, scalaJars210.toList, classDirectory210)
     val compilerBridge211 = ScalaBridge(scalaVersion211, scalaJars211.toList, classDirectory211)
     val compilerBridge212 = ScalaBridge(scalaVersion212, scalaJars212.toList, classDirectory212)
     val compilerBridge213 = ScalaBridge(scalaVersion213, scalaJars213.toList, classDirectory213)
-    List(compilerBridge210, compilerBridge211, compilerBridge212, compilerBridge213)
+    val compilerBridge213_next =
+      ScalaBridge(scalaVersion213_next, scalaJars213_next.toList, classDirectory213_next)
+    List(
+      compilerBridge210,
+      compilerBridge211,
+      compilerBridge212,
+      compilerBridge213,
+      compilerBridge213_next
+    )
   }
 
   // Create a provider that uses the bridges from the classes directory of the projects
