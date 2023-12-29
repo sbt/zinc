a9846ba0d3dcd543aec1fc9ac6abeb8d8805eb41
friendseeker <66892505+Friendseeker@users.noreply.github.com>
2023-12-29 01:22:38 -0800

    Mark macro-type-change-3 as passing
    The reason the test failed was not because of issue with transitive external invalidation, but rather the macro itself is the issue.

--- zinc/src/sbt-test/macros/macro-type-change-3/macros/Macros.scala
+++ zinc/src/sbt-test/macros/macro-type-change-3/macros/Macros.scala
@@ -10,10 +10,11 @@ object Macros {
   def hasAnyFieldImpl[T: c.WeakTypeTag](c: Context): c.Expr[Boolean] = {
     import c.universe._
 
-    val hasField = weakTypeOf[T].members.exists {
-      case m: TermSymbol => m.isVal || m.isVar
-      case _ => false
-    }
+    val hasField = weakTypeOf[T].baseClasses.flatMap(base =>
+      base.asType.toType.decls.collect {
+        case m: TermSymbol if m.isVal || m.isVar => m
+      }
+    ).nonEmpty
 
     c.Expr[Boolean](Literal(Constant(hasField)))
   }
