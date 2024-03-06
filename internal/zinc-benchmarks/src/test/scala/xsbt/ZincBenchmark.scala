8d13d76694b702f32f4b2fb86d4e6cd2fd558c54
Scala Steward <scala_steward@virtuslab.com>
2024-03-06 22:02:35 +0000

    Update scala-compiler, scala-library to 2.13.13

--- internal/zinc-benchmarks/src/test/scala/xsbt/ZincBenchmark.scala
+++ internal/zinc-benchmarks/src/test/scala/xsbt/ZincBenchmark.scala
@@ -107,7 +107,7 @@ private[xsbt] class ZincBenchmark(toCompile: BenchmarkProject, zincEnabled: Bool
 private[xsbt] object ZincBenchmark {
   // This is the Scala version used to compile the benchmark project
   // do not use `scala.util.Properties.versionNumberString`.
-  val scalaVersion = "2.13.12"
+  val scalaVersion = "2.13.13"
 
   /* ************************************************************* */
   /* Utils to programmatically instantiate Compiler from sbt setup  */
