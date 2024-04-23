5cda61cda2865374b0dd1b99377ac52abcf623ab
Scala Steward <scala_steward@virtuslab.com>
2024-04-23 19:21:28 +0000

    Update scala-parser-combinators to 2.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -72,7 +72,7 @@ object Dependencies {
   val scalaLibrary = Def.setting { "org.scala-lang" % "scala-library" % scalaVersion.value }
   val scalaCompiler = Def.setting { "org.scala-lang" % "scala-compiler" % scalaVersion.value }
 
-  val parserCombinator = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
+  val parserCombinator = "org.scala-lang.modules" %% "scala-parser-combinators" % "2.4.0"
   val sbinary = "org.scala-sbt" %% "sbinary" % "0.5.1"
   val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.2.0"
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.17.0"
