331716e861907877d51b5f69bb71321bfa2ef716
Scala Steward <scala_steward@virtuslab.com>
2024-09-22 17:10:13 +0000

    Update util-control, util-interface, ... to 1.10.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val scala212_213 = Seq(defaultScalaVersion, scala213)
 
   private val ioVersion = nightlyVersion.getOrElse("1.10.0")
-  private val utilVersion = nightlyVersion.getOrElse("1.10.1")
+  private val utilVersion = nightlyVersion.getOrElse("1.10.2")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
