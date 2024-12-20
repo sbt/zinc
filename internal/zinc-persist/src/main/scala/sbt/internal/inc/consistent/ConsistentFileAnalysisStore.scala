dad906b52b730bb1730028355d611a5f2755985d
friendseeker <66892505+Friendseeker@users.noreply.github.com>
2024-12-20 01:57:17 -0800

    Remove duplicated copyright

--- internal/zinc-persist/src/main/scala/sbt/internal/inc/consistent/ConsistentFileAnalysisStore.scala
+++ internal/zinc-persist/src/main/scala/sbt/internal/inc/consistent/ConsistentFileAnalysisStore.scala
@@ -12,17 +12,6 @@
 // Based on zinc's FileAnalysisStore:
 package sbt.internal.inc.consistent
 
-/*
- * Zinc - The incremental compiler for Scala.
- * Copyright Lightbend, Inc. and Mark Harrah
- *
- * Licensed under Apache License 2.0
- * (http://www.apache.org/licenses/LICENSE-2.0).
- *
- * See the NOTICE file distributed with this work for
- * additional information regarding copyright ownership.
- */
-
 import sbt.io.{ IO, Using }
 import xsbti.compile.analysis.ReadWriteMappers
 import xsbti.compile.{ AnalysisContents, AnalysisStore => XAnalysisStore }
