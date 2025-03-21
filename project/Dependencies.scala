07c09c797181e496241e84955859ac524b34eeaf
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-03-22 00:08:56 +0100

    Update util-control, util-interface, ... to 1.10.11
    Co-authored-by: eugene yokota <eed3si9n@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val scala212_213 = Seq(defaultScalaVersion, scala213)
 
   private val ioVersion = nightlyVersion.getOrElse("1.10.5")
-  private val utilVersion = nightlyVersion.getOrElse("1.10.7")
+  private val utilVersion = nightlyVersion.getOrElse("1.10.11")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
