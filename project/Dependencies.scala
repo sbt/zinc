76be4e8b4e3774fa5fea19ab0e4f54545063f935
Scala Steward <scala_steward@virtuslab.com>
2025-11-14 18:49:00 +0000

    Update scala3-library to 3.7.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val scala211 = "2.11.12"
   val scala212 = "2.12.20"
   val scala213 = "2.13.16"
-  val scala3 = "3.7.3"
+  val scala3 = "3.7.4"
   val scala3ForBridge = "3.3.4"
   val scala213ForBridge = "2.13.16"
   val defaultScalaVersion = scala3
