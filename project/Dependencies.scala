ee6e8502d9a6d97481e8fd3911a475e5c9900e17
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-02-27 05:23:17 +0100

    Update util-control, util-interface, ... to 2.0.0-RC9 (#1648)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val scala3_only = Seq(scala3)
 
   private val ioVersion = nightlyVersion.getOrElse("1.10.5")
-  private val utilVersion = nightlyVersion.getOrElse("2.0.0-RC8")
+  private val utilVersion = nightlyVersion.getOrElse("2.0.0-RC9")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
